23ce5d7e9bc35b778d5bec373642b948407af002
Vasileios Mylonakis <milonakisv@interamerican.gr>
2013-10-21 08:00:23 +0000

    fixed warnings
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2639 6845aa9c-c3df-f248-9547-742189ef8f90

--- WicketBo2/main/gr/interamerican/wicket/bo2/protocol/http/Bo2WicketRequestCycle.java
+++ WicketBo2/main/gr/interamerican/wicket/bo2/protocol/http/Bo2WicketRequestCycle.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.wicket.bo2.protocol.http;
@@ -27,7 +27,6 @@ import gr.interamerican.bo2.arch.exceptions.DataException;
 import gr.interamerican.bo2.arch.exceptions.InitializationException;
 import gr.interamerican.bo2.arch.exceptions.LogicException;
 import gr.interamerican.bo2.arch.ext.Session;
-import gr.interamerican.bo2.arch.utils.Bo2ArchUtils;
 import gr.interamerican.bo2.arch.utils.ext.Bo2Session;
 import gr.interamerican.bo2.impl.open.creation.Factory;
 import gr.interamerican.bo2.impl.open.namedstreams.NamedStreamsProvider;
@@ -60,12 +59,12 @@ import org.slf4j.LoggerFactory;
  * 
  * This implementation of WebRequestCycle creates a transaction
  * for each request. A {@link TransactionManager} is used for that.
- * The WebRequestCycle offers a {@link Provider} property that can 
+ * The WebRequestCycle offers a {@link Provider} property that can
  * be used by any {@link Worker}.
  */
 @SuppressWarnings("nls")
 public class Bo2WicketRequestCycle extends WebRequestCycle {
-	
+
 	/**
 	 * Calls <code>onBeginRequest()</code> on the specified cycle.
 	 * 
@@ -73,13 +72,13 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * classes in order to emulate the wicket request-response cycle.
 	 * 
 	 * @param cycle
-	 *        {@link Bo2WicketRequestCycle} on which the method is 
+	 *        {@link Bo2WicketRequestCycle} on which the method is
 	 *        invoked.
 	 */
 	public static void beginRequest(Bo2WicketRequestCycle cycle) {
 		cycle.onBeginRequest();
 	}
-	
+
 	/**
 	 * Calls <code>onEndRequest()</code> on the specified cycle.
 	 * 
@@ -87,13 +86,13 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * classes in order to emulate the wicket request-response cycle.
 	 * 
 	 * @param cycle
-	 *        {@link Bo2WicketRequestCycle} on which the method is 
+	 *        {@link Bo2WicketRequestCycle} on which the method is
 	 *        invoked.
 	 */
 	public static void endRequest(Bo2WicketRequestCycle cycle) {
 		cycle.onEndRequest();
 	}
-	
+
 	/**
 	 * Creates an object using the default Bo2 Factory.
 	 * 
@@ -105,14 +104,14 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	public static final <P> P create(Class<P> clazz) {
 		return Factory.create(clazz);
 	}
-	
+
 	/**
 	 * Initializes and opens the specified worker.
 	 * 
 	 * @param w Worker to init and open.
 	 */
 	public static void initAndOpen(Worker w) {
-		try {			
+		try {
 			w.init(get().getProvider());
 			w.open();
 			get().workers.add(w);
@@ -122,11 +121,11 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			throw new RuntimeException(de);
 		}
 	}
-	
+
 	/**
 	 * Gets an open {@link Worker} object of a specified type.
 	 * 
-	 * Any Exception thrown during the operation is wrapped inside a 
+	 * Any Exception thrown during the operation is wrapped inside a
 	 * {@link RuntimeException}.
 	 * 
 	 * @param clazz Class of worker.
@@ -138,47 +137,47 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 		W w = Factory.create(clazz);
 		initAndOpen(w);
 		return w;
-	}	
-	
+	}
+
 	/**
 	 * Gets an open {@link PersistenceWorker} for {@link PersistentObject}
 	 * objects of type P.
 	 * 
-	 * Any Exception thrown during the operation is wrapped inside a 
+	 * Any Exception thrown during the operation is wrapped inside a
 	 * {@link RuntimeException}.
 	 * 
 	 * @param clazz Class of {@link PersistentObject}.
-	 * @param <P> Type of {@link PersistentObject} defined by 
+	 * @param <P> Type of {@link PersistentObject} defined by
 	 *        <code>clazz</code>.
 	 * 
 	 * @return Returns a new open PersistenceWorker for P objects.
 	 */
-	public static final <P extends PersistentObject<?>> 
+	public static final <P extends PersistentObject<?>>
 	PersistenceWorker<P> openPw(Class<P> clazz) {
-		PersistenceWorker<P> w = Factory.createPw(clazz);		
+		PersistenceWorker<P> w = Factory.createPw(clazz);
 		initAndOpen(w);
 		return w;
 	}
-	
+
 	/**
 	 * Executes an operation.
 	 * 
-	 * Any Exception thrown during the operation is wrapped inside a 
+	 * Any Exception thrown during the operation is wrapped inside a
 	 * {@link RuntimeException}.
 	 * 
 	 * @param op Operation to execute.
 	 */
 	public static final void execute(Operation op) {
 		initAndOpen(op);
-		try {		
+		try {
 			op.execute();
 		} catch (DataException de) {
 			throw new RuntimeException(de);
 		} catch (LogicException le) {
 			throw new RuntimeException(le);
 		}
-	}	
-	
+	}
+
 
 	/**
 	 * Gets the default NamedStreamsProvider defined in the deployment
@@ -188,15 +187,15 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * 
 	 * @throws InitializationException
 	 */
-	public static final NamedStreamsProvider getDefaultNamedStreamsProvider() 
-	throws InitializationException {
-		Bo2DeploymentParams depl =Bo2.getDefaultDeployment().getDeploymentBean();  
+	public static final NamedStreamsProvider getDefaultNamedStreamsProvider()
+			throws InitializationException {
+		Bo2DeploymentParams depl =Bo2.getDefaultDeployment().getDeploymentBean();
 		String nspName = depl.getStreamsManagerName();
-		NamedStreamsProvider nsp = 
-			provider().getResource(nspName, NamedStreamsProvider.class);
+		NamedStreamsProvider nsp =
+				provider().getResource(nspName, NamedStreamsProvider.class);
 		return nsp;
-	}	
-	
+	}
+
 	/**
 	 * Gets the default NamedStreamsProvider defined in the deployment
 	 * properties.
@@ -205,33 +204,33 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * 
 	 * @throws InitializationException
 	 */
-	public static final StreamsProvider getDefaultStreamsProvider() 
-	throws InitializationException {
-		Bo2DeploymentParams depl = Bo2.getDefaultDeployment().getDeploymentBean(); 
+	public static final StreamsProvider getDefaultStreamsProvider()
+			throws InitializationException {
+		Bo2DeploymentParams depl = Bo2.getDefaultDeployment().getDeploymentBean();
 		String nspName = depl.getStreamsManagerName();
 		StreamsProvider sp = provider().getResource(nspName, StreamsProvider.class);
 		return sp;
-	}	
-	
+	}
+
 	/**
 	 * This method will reattach a dettached object to the current hibernate
 	 * session.
 	 * 
-	 * The method is a facade that hides the hibernate session. If the current 
+	 * The method is a facade that hides the hibernate session. If the current
 	 * deployment does not support hibernate, the method will do nothing.
 	 * If the specified object was not managed by hibernate, then nothing will
-	 * happen. Only if the specified object was managed by hibernate, will it 
+	 * happen. Only if the specified object was managed by hibernate, will it
 	 * be attached to the session. This behavior guarantees that the application
-	 * will run even if the specified object's persistence worker, changes and is 
+	 * will run even if the specified object's persistence worker, changes and is
 	 * not longer based on hibernate.
 	 * 
 	 * @param object
 	 *        The object to re-attach
 	 */
 	public static final void reattach(Object object) {
-		PoUtils.reattach(object, Bo2WicketRequestCycle.get().getProvider());		
-	}	
-	
+		PoUtils.reattach(object, Bo2WicketRequestCycle.get().getProvider());
+	}
+
 	/**
 	 * Gets the provider of the {@link Bo2WicketRequestCycle} associated
 	 * with the current thread.
@@ -239,53 +238,53 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * @return Returns the provider.
 	 */
 	public static final Provider provider() {
-		return get().getProvider();		
-	}	
-	
-	
+		return get().getProvider();
+	}
+
+
 	public static Bo2WicketRequestCycle get() {
 		return (Bo2WicketRequestCycle) RequestCycle.get();
 	}
-	
+
 	/**
 	 * logger.
 	 */
 	private static Logger logger = LoggerFactory.getLogger(Bo2WicketRequestCycle.class);
-	
+
 	/**
 	 * RequestCycleStats.
 	 */
 	private static RequestCycleStats stats = new RequestCycleStats();
-	
+
 	/**
 	 * Resource manager for the operation.
 	 */
 	private Provider provider;
-		
+
 	/**
-	 * Workers managed by this request cycle. 
+	 * Workers managed by this request cycle.
 	 */
 	private List<Worker> workers = new ArrayList<Worker>();
-	
+
 	/**
 	 * Names of workers.
 	 */
 	private String workerNames = null;
-	
+
 	/**
 	 * Indicates if an error has happened in the current cycle.
 	 */
 	private boolean error = false;
-	
+
 	/**
 	 * Timer.
 	 */
 	private Timer timer;
-	
 
-	
+
+
 	/**
-	 * Creates a new Bo2WicketRequestCycle object. 
+	 * Creates a new Bo2WicketRequestCycle object.
 	 *
 	 * @param application
 	 * @param request
@@ -295,7 +294,7 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	(WebApplication application, WebRequest request, Response response) {
 		super(application, request, response);
 	}
-	
+
 	@Override
 	protected void onRequestTargetSet(IRequestTarget requestTarget) {
 		super.onRequestTargetSet(requestTarget);
@@ -313,7 +312,7 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			}
 		}
 	}
-	
+
 	@Override
 	protected void onBeginRequest() {
 		stats.newCycle();
@@ -326,16 +325,16 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			TransactionManager manager = provider.getTransactionManager();
 			if(manager!=null) {
 				manager.begin();
-				debug("started the transaction manager " + this.toString());
+				debug("started the transaction manager " + toString());
 			}
 		} catch (CouldNotBeginException e) {
-			throw new RuntimeException(e);			
+			throw new RuntimeException(e);
 		} catch (InitializationException e) {
 			throw new RuntimeException(e);
 		}
 		super.onBeginRequest();
 	}
-	
+
 	@Override
 	protected void onEndRequest() {
 		/*
@@ -352,32 +351,32 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 				}
 			} catch (CouldNotCommitException cnce) {
 				logger.error("CouldNotCommitException: " + ExceptionUtils.getThrowableStackTrace(cnce));
-				throw new RuntimeException(cnce);		
+				throw new RuntimeException(cnce);
 			} finally {
 				try {
 					cleanup();
 				}catch (DataException de) {
 					logger.error("DataException on cleanup: " + ExceptionUtils.getThrowableStackTrace(de));
-					throw new RuntimeException(de);		
+					throw new RuntimeException(de);
 				}
 			}
-		} else {			
-			debug("Nothing to do, an error was registered in the cycle.");			
+		} else {
+			debug("Nothing to do, an error was registered in the cycle.");
 		}
 		super.onEndRequest();
 		logAndCleanSession();
 	}
-	
+
 	@Override
 	public Page onRuntimeException(Page page, RuntimeException e) {
-		Throwable t = Bo2ArchUtils.unwrap(e);
+		Throwable t = ExceptionUtils.unwrap(e);
 		stats.updateExceptionStats(t);
 		error = true;
 		try {
 			TransactionManager manager = provider.getTransactionManager();
 			if(manager!=null) {
-				manager.rollback();				
-				debug("rolled back the transaction.");				
+				manager.rollback();
+				debug("rolled back the transaction.");
 			}
 		} catch (CouldNotRollbackException cnrbex) {
 			cnrbex.setInitial(t);
@@ -398,10 +397,10 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			InvocationTargetException itex = ExceptionUtils.causeInTheChain(t, InvocationTargetException.class);
 			t = Utils.notNull(itex.getTargetException(), t);
 		}
-		
+
 		if (page instanceof WicketOutputMedium) {
 			WicketOutputMedium outputMedium = (WicketOutputMedium) page;
-			IRequestTarget target = getRequestTarget(); 
+			IRequestTarget target = getRequestTarget();
 			if(target instanceof AjaxRequestTarget) {
 				AjaxRequestTarget art = (AjaxRequestTarget) target;
 				outputMedium.showError(t, art);
@@ -411,11 +410,11 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			logAndCleanSession();
 			return page;
 		} else {
-			logAndCleanSession();			
+			logAndCleanSession();
 			return superOnRuntimeException(page, e);
 		}
 	}
-	
+
 	/**
 	 * Marks the specified object as being saved only when explicitly defined
 	 * by its persistent worker.
@@ -427,25 +426,25 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	 * result in the specified object being excluded from flush. Calling this
 	 * method will also change the flushing policy of the current cycle's
 	 * HibernateSessionProvider to FlushStrategy#EXCLUDING. <br/>
-	 * If the current deployment does not support hibernate or the specified 
-	 * object is not managed by hibernate, then this method will have no effect. 
-	 *  
+	 * If the current deployment does not support hibernate or the specified
+	 * object is not managed by hibernate, then this method will have no effect.
+	 * 
 	 * @param object
 	 *        Object to mark.
 	 */
 	public void markExplicitSave(Object object) {
 		DetachStrategy strategy = PoUtils.getDetachStrategy(object);
 		if (strategy!=null) {
-			strategy.markExplicitSave(object, this.getProvider());
+			strategy.markExplicitSave(object, getProvider());
 		}
 	}
-	
+
 	/**
 	 * Calls super.OnRuntimeException().
 	 * 
 	 * @param page
 	 * @param e
-	 * @return Returns the page returned by super.OnRuntimeException() 
+	 * @return Returns the page returned by super.OnRuntimeException()
 	 */
 	private Page superOnRuntimeException(Page page, RuntimeException e) {
 		logger.error("UNEXPECTED EXCEPTION: " + ExceptionUtils.getThrowableStackTrace(e));
@@ -457,33 +456,33 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 		Bo2Session.setSession(null);
 		return errorPage;
 	}
-	
+
 	/**
 	 * Closes any workers opened in this cycle and the provider.
 	 * 
-	 * @throws DataException 
+	 * @throws DataException
 	 */
-	private void cleanup() throws DataException {		
-		debug("performing cleanup.");			
+	private void cleanup() throws DataException {
+		debug("performing cleanup.");
 		closeWorkers();
-		provider.close();		
+		provider.close();
 		debug("cleaned up.");
 	}
-	
+
 	/**
 	 * Closes any worker opened during this cycle.
 	 * 
-	 * @throws DataException 
+	 * @throws DataException
 	 */
-	private void closeWorkers() throws DataException {	
+	private void closeWorkers() throws DataException {
 		StringBuilder sb = new StringBuilder();
 		for (Worker worker : workers) {
 			worker.close();
 			sb.append(worker.getClass().getSimpleName());
 			sb.append(COMMA);
 		}
-		workers.clear();			
-		this.workerNames = sb.toString();
+		workers.clear();
+		workerNames = sb.toString();
 	}
 
 	/**
@@ -494,7 +493,7 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 	public Provider getProvider() {
 		return provider;
 	}
-	
+
 	/**
 	 * Cleanup and logging.
 	 */
@@ -503,7 +502,7 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 		Bo2Session.setSession(null);
 		Bo2Session.setProvider(null);
 	}
-	
+
 	/**
 	 * Writes a debug message through the logger.
 	 * 
@@ -514,7 +513,7 @@ public class Bo2WicketRequestCycle extends WebRequestCycle {
 			logger.debug(msg);
 		}
 	}
-	
+
 
 }
 
