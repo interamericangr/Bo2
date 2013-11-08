f77a9277543f9ef58d805dbbc00dfe330e871237
Vasileios Mylonakis <milonakisv@interamerican.gr>
2013-11-08 12:37:05 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2695 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ releaseBo.sh
@@ -0,0 +1,10 @@
+mvn release:prepare
+e="$?"
+if [ $e -ne 0 ]
+then
+	echo "mvn build failed exiting..."
+	exit -1
+fi
+v=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+mvn release:prepare -Darguments="-DskipTests"
+mail -s "Neo BO2 $v" `cat recipientsOne.txt`</dev/null
