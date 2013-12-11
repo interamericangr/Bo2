5d42cbc2325332ff5cd2b50d94d60e654c60babd
Dimitris Kateros <katerosd@interamerican.gr>
2013-12-11 14:10:41 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2734 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2ImplOpen/test/gr/interamerican/bo2/impl/open/jdbc/TestJdbcCustomAnswerQuestion.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/copyleft/lesser.html
+ * 
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Lesser General Public License for more details.
+ ******************************************************************************/
+package gr.interamerican.bo2.impl.open.jdbc;
+
+import static org.junit.Assert.assertTrue;
+import gr.interamerican.bo2.arch.exceptions.DataAccessException;
+import gr.interamerican.bo2.arch.exceptions.DataException;
+import gr.interamerican.bo2.arch.exceptions.InitializationException;
+import gr.interamerican.bo2.arch.exceptions.LogicException;
+import gr.interamerican.bo2.impl.open.annotations.ManagerName;
+import gr.interamerican.bo2.impl.open.annotations.Parameter;
+import gr.interamerican.bo2.test.utils.UtilityForBo2Test;
+
+import org.junit.Test;
+
+/**
+ * Test for {@link JdbcCustomAnswerQuestion}.
+ */
+public class TestJdbcCustomAnswerQuestion extends AbstractNonTransactionalProviderTest {
+	
+	/**
+	 * tests the question that uses named parameters.
+	 * 
+	 * @throws InitializationException 
+	 * @throws LogicException 
+	 * @throws DataException 
+	 */
+	@Test
+	public void testExecuteQuestion_withNamedParams() 
+	throws InitializationException, DataException, LogicException {	
+		Integer id = UtilityForBo2Test.getExistingUserId();
+		JdbcCustomAnswerQuestionSample question = new JdbcCustomAnswerQuestionSample();
+		question.init(provider);
+		question.open();		
+		question.id = id;
+		question.ask();
+		Boolean answer = question.getAnswer();
+		question.close();
+		
+		assertTrue(answer);
+		
+	}
+	
+	/**
+	 * implementation to test
+	 */
+	@ManagerName("LOCALDB")
+	private class JdbcCustomAnswerQuestionSample 
+	extends JdbcCustomAnswerQuestion<Boolean> {	
+		
+		/**
+		 * id parameter.
+		 */		
+		@Parameter Integer id;
+		
+		/**
+		 * sgl.
+		 */		
+		@Sql private static final String sql = "select 1 from X__X.users where id = :id"; //$NON-NLS-1$	
+		
+		@Override
+		protected Boolean createAnswer() throws DataAccessException {
+			return getBoolean(1); 
+		}
+		
+		@Override
+		public Boolean getAnswer() {
+			if(super.getAnswer()==null) {
+				return false;
+			}
+			return super.getAnswer();
+		}
+		
+	}
+	
+}
