6dc4535a1372486dab5e33a0f966e19e72684a63
Vasileios Mylonakis <milonakisv@interamerican.gr>
2014-01-16 09:32:18 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2755 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplHibernate/test/gr/interamerican/bo2/impl/open/hibernate/utils/reflect/analyze/TestSuiteBo2ImplOpenHibernateUtilsReflectAnalyze.java
+++ Bo2ImplHibernate/test/gr/interamerican/bo2/impl/open/hibernate/utils/reflect/analyze/TestSuiteBo2ImplOpenHibernateUtilsReflectAnalyze.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.impl.open.hibernate.utils.reflect.analyze;
@@ -23,15 +23,16 @@ import org.junit.runners.Suite.SuiteClasses;
  * Test suite for package <code>gr.interamerican.bo2.impl.open.hibernate</code>.
  *
  */
+@SuppressWarnings("deprecation")
 @RunWith(Suite.class)
 @SuiteClasses(
-	{	
-		TestModificationRecordFieldsAnalyzer.class,
-		TestHibernateAwarePoAnalyzer.class
-	}
-)
+		{
+			TestModificationRecordFieldsAnalyzer.class,
+			TestHibernateAwarePoAnalyzer.class
+		}
+		)
 public class TestSuiteBo2ImplOpenHibernateUtilsReflectAnalyze {
-	/* empty */	
+	/* empty */
 }
 
 
