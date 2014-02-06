0c404a188c053de877f0ff1d932d1d7d2674e4ba
Dimitris Kateros <katerosd@interamerican.gr>
2014-02-06 10:50:24 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2807 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplHibernate/main/gr/interamerican/bo2/impl/open/hibernate/adapters/Unproxy.java
+++ Bo2ImplHibernate/main/gr/interamerican/bo2/impl/open/hibernate/adapters/Unproxy.java
@@ -19,8 +19,21 @@ import org.hibernate.proxy.HibernateProxy;
 
 /**
  * Initializes a hibernate proxy.
+ * <br/>
+ * {@link HibernateProxy} implementations created on runtime by hibernate
+ * do not delegate equals() and hashCode() to the wrapped entity instance.
+ * For this reason, in scenarios where the entities override equals() and
+ * hashCode() we have to unwrap the instance manually (e.g. when working 
+ * with Collections).
+ * <br/>
+ * However, note that having to use this something fishy is happening. Most
+ * likely there exist simultaneously a HibernateProxy AND an entity instance
+ * with the same database id in the scope of your unit of work. In a scenario
+ * with detached entities, if everything is reattached properly to the next
+ * hibernate Session at the beginning of the unit of work, there should be no
+ * problem. 
  */
-public class Unproxy implements Modification<PersistentObject<?>>{
+public class Unproxy implements Modification<PersistentObject<?>> {
 	
 	/**
 	 * Singleton.
