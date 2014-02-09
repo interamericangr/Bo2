0909f6f1c70040fa9ba55c4316cbc60d146c6c09
Spyros Nakos <nakoss@interamerican.gr>
2014-02-09 01:29:59 +0000

    new attributes
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2822 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2Utils/main/gr/interamerican/bo2/utils/attributes/Input.java
@@ -0,0 +1,24 @@
+package gr.interamerican.bo2.utils.attributes;
+
+/**
+ * {@link Input} models any class that requires an input.
+ * 
+ * @param <I>
+ *        Type of input object. 
+ */
+public interface Input<I> {
+	/**
+	 * Sets the input.
+	 * 
+	 * @param input
+	 */
+	public void setInput(I input);
+	
+	/**
+	 * Gets the input.
+	 * 
+	 * @return returns the input.
+	 */
+	public I getInput();
+
+}
---
+++ Bo2Utils/main/gr/interamerican/bo2/utils/attributes/NumericCalculator.java
@@ -0,0 +1,16 @@
+package gr.interamerican.bo2.utils.attributes;
+
+/**
+ * {@link NumericCalculator} models a class that calculates
+ * a Number.
+ */
+public interface NumericCalculator {
+	
+	/**
+	 * Gets the numeric result of this calculation.
+	 * 
+	 * @return Returns the result.
+	 */
+	Number getNumericResult();
+
+}
