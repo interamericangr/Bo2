1f095db45bf79e3e49799f5a6a6baaf9a195b11b
Spyros Nakos <nakoss@interamerican.gr>
2014-02-16 03:53:17 +0000

    new transformations
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2848 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2Utils/samples/gr/interamerican/bo2/samples/bean/BeanWithString.java
@@ -0,0 +1,31 @@
+package gr.interamerican.bo2.samples.bean;
+
+/**
+ * Bean with a string property.
+ */
+public class BeanWithString {
+	
+	/**
+	 * String.
+	 */
+	String string;
+
+	/**
+	 * Gets the string.
+	 *
+	 * @return Returns the string
+	 */
+	public String getString() {
+		return string;
+	}
+
+	/**
+	 * Assigns a new value to the string.
+	 *
+	 * @param string the string to set
+	 */
+	public void setString(String string) {
+		this.string = string;
+	}
+
+}
