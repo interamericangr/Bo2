106d0a4a6bfd59b94d65af008c82be55d9d82519
Spyros Nakos <nakoss@interamerican.gr>
2014-02-16 03:53:45 +0000

    new transformations
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2849 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2Utils/main/gr/interamerican/bo2/utils/adapters/InvokeMethod.java
@@ -0,0 +1,100 @@
+package gr.interamerican.bo2.utils.adapters;
+
+import gr.interamerican.bo2.utils.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+
+/**
+ * {@link InvokeMethod} is a generic adapter that 
+ * 
+ * 
+ * @param <A>
+ *        Type of operation argument.
+ * @param <R> 
+ *        Type of operation result.
+ */
+public class InvokeMethod<A, R>
+implements Transformation<A, R> {
+	
+	/**
+	 * Class that has the method to invoke.
+	 */
+	Class<?> clazz;
+
+	/**
+	 * method to be invoked.
+	 */
+	Method method;
+	
+	/**
+	 * Method name.
+	 */
+	String methodName;
+	
+	/**
+	 * Target object.
+	 */
+	Object target;
+	
+	/**
+	 * Argument type.
+	 */
+	Class<?> argumentType;
+	
+	/**
+	 * Creates a new InvokeMethod object. 
+	 *
+	 * @param clazz
+	 *        Class that has a public method that will be invoked.
+	 * @param methodName
+	 *        name of public method.
+	 * @param argumentType
+	 *        Argument type of the method. The method must have only one argument.
+	 * @param target
+	 *        Target object on which the method will be invoked.
+	 * 
+	 *        
+	 */
+	public InvokeMethod(Class<?> clazz, String methodName, Class<?> argumentType, Object target) {
+		super();
+		this.clazz = clazz;
+		this.methodName = methodName;
+		this.target = target;
+		this.argumentType = argumentType;
+		try {
+			method = clazz.getMethod(methodName, argumentType);		 
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Creates a new InvokeMethod object for a static method. 
+	 *
+	 * @param clazz
+	 *        Class that has a publicstatic  method that will be invoked.
+	 * @param methodName
+	 *        name of public static method.
+	 * @param argumentType
+	 *        Argument type of the method. The method must have only one argument.
+	 *        
+	 */
+	public InvokeMethod(Class<?> clazz, String methodName, Class<?> argumentType) {
+		this(clazz, methodName, argumentType, null);
+		if (!Modifier.isStatic(method.getModifiers())) {
+			String msg = "Method " + methodName + " is not static"; //$NON-NLS-1$ //$NON-NLS-2$
+			throw new RuntimeException(msg);
+		}
+		
+	}
+		
+	@SuppressWarnings("unchecked")
+	public R execute(A a) {
+		return (R) ReflectionUtils.invoke(method, target, a);
+	}
+
+	
+	
+}
