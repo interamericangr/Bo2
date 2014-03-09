3718e975aea2be0d0fdc91fe7fe9a237630a7ffd
Andreas Maniatakos <maniatakosan@interamerican.gr>
2014-03-09 18:48:55 +0000

    multipart form reverted!
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@2973 6845aa9c-c3df-f248-9547-742189ef8f90

--- WicketUtils/main/gr/interamerican/wicket/markup/html/panel/bean/SingleBeanPanel.java
+++ WicketUtils/main/gr/interamerican/wicket/markup/html/panel/bean/SingleBeanPanel.java
@@ -184,7 +184,6 @@ extends ServicePanelWithBack {
 
 		beanForm = new Form<B>(FORM_ID, getDefinition().getBeanModel());
 		beanForm.setOutputMarkupId(true);
-		beanForm.setMultiPart(true);
 		if(getDefinition().getSingleBeanFormContainsFileUpload()) {
 			beanForm.setMultiPart(true);
 			beanForm.add(new AttributeModifier("enctype", new Model<String>("multipart/form-data")));
