69e5b80f0d80c7bda48e22d9edf3c7cbfed0a662
Dimitris Kateros <katerosd@interamerican.gr>
2014-04-03 09:58:56 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3078 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2Utils/main/gr/interamerican/bo2/utils/inspect/FieldInspector.java
@@ -0,0 +1,134 @@
+package gr.interamerican.bo2.utils.inspect;
+
+import gr.interamerican.bo2.utils.ReflectionUtils;
+import gr.interamerican.bo2.utils.StringConstants;
+import gr.interamerican.bo2.utils.StringUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 
+ */
+public abstract class FieldInspector {
+	
+	/**
+	 * Hashcodes of inspected objects.
+	 */
+	private Set<Integer> alreadyInspected = new HashSet<Integer>();
+	
+	/**
+	 * Inspect the specified instance.
+	 * @param obj
+	 */
+	public void inspect(Object obj) {
+		alreadyInspected.clear();
+		inspect0(obj);
+	}
+	
+	/**
+	 * 
+	 * @param obj
+	 */
+	void inspect0(Object obj) {
+		if(obj==null){
+			return;
+		}
+		
+		if(!alreadyInspected.add(System.identityHashCode(obj))) {
+			return;
+		}
+		
+		List<Field> fields = ReflectionUtils.allFields(obj.getClass(), Object.class);
+		
+		for(Field field : fields) {
+			if(!shouldInspectField(field)) {
+				continue;
+			}
+			
+			field.setAccessible(true);
+			
+			Object fieldValue = ReflectionUtils.get(field, obj);
+			
+			if(fieldValue instanceof Iterable) {
+				Iterable<?> it = (Iterable<?>) fieldValue;
+				for(Object element : it) {
+					if(shouldReportFieldValue(element)) {
+						doReport(obj, field, element, true);
+					}
+					inspect0(element);
+				}
+			}
+			
+			if(fieldValue instanceof Object[]) {
+				Object[] ar = (Object[]) fieldValue;
+				for(Object element : ar) {
+					if(shouldReportFieldValue(element)) {
+						doReport(obj, field, element, true);
+					}
+					inspect0(element);
+				}
+			}
+			
+			if(shouldReportFieldValue(fieldValue)) {
+				doReport(obj, field, fieldValue, false);
+			}
+			
+			inspect0(fieldValue);
+			
+		}
+	}
+	
+	/**
+	 * Default reporting action.
+	 * 
+	 * @param inspected
+	 * @param f
+	 * @param fieldValue
+	 * @param owned
+	 */
+	protected void doReport(Object inspected, Field f, Object fieldValue, boolean owned) {
+		System.out.println(reportMessage(inspected, f, fieldValue, owned));
+	}
+	
+	/**
+	 * Default reporting message.
+	 * 
+	 * @param inspected
+	 * @param f
+	 * @param fieldValue
+	 * @param owned
+	 * @return reporting message.
+	 */
+	@SuppressWarnings("nls")
+	protected String reportMessage(Object inspected, Field f, Object fieldValue, boolean owned) {
+		String msg = StringUtils.concat(
+			Modifier.toString(f.getModifiers()),
+			StringConstants.SPACE,
+			inspected.getClass().getSimpleName(),
+			StringConstants.DOT,
+			f.getName(),
+			StringConstants.SPACE + StringConstants.COLON + StringConstants.SPACE,
+			fieldValue==null ? "null" : fieldValue.getClass().getSimpleName());
+		if(owned) {
+			msg += " (owned array or collection element)";
+		}
+		return msg;
+	}
+	
+	/**
+	 * @param field
+	 * @return Whether this field should be inspected
+	 */
+	protected abstract boolean shouldInspectField(Field field);
+	
+	/**
+	 * @param fieldValue
+	 * @return Whether this field value should be included in the report
+	 */
+	protected abstract boolean shouldReportFieldValue(Object fieldValue);
+	
+}
---
+++ Bo2Utils/test/gr/interamerican/bo2/utils/inspect/TestFieldInspector.java
@@ -0,0 +1,55 @@
+package gr.interamerican.bo2.utils.inspect;
+
+import gr.interamerican.bo2.samples.Child;
+import gr.interamerican.bo2.utils.StringUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test {@link FieldInspector}
+ */
+public class TestFieldInspector {
+	
+	/**
+	 * test inspect()
+	 */
+	@Test
+	public void testInspect() {
+		PrintStream out = System.out;
+		try {
+			ByteArrayOutputStream pipeOut = new ByteArrayOutputStream();
+			System.setOut(new PrintStream(pipeOut));
+			new FieldInspectorImpl().inspect(new Child());
+			String output = new String(pipeOut.toByteArray());
+			
+			Assert.assertFalse(StringUtils.isNullOrBlank(output));
+			Assert.assertFalse(output.contains("static")); //$NON-NLS-1$
+		} finally {
+			System.setOut(out);
+		}
+		
+	}
+	
+	/**
+	 * Sample impl that omits static fields
+	 */
+	static class FieldInspectorImpl extends FieldInspector {
+		
+		@Override
+		protected boolean shouldReportFieldValue(Object fieldValue) {
+			return true;
+		}
+		
+		@Override
+		protected boolean shouldInspectField(Field field) {
+			return !Modifier.isStatic(field.getModifiers());
+		}
+	}
+
+}
