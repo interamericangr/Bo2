9e1d719b2f3f1fc59511d9b60b5295b114de683a
Dimitris Kateros <katerosd@interamerican.gr>
2014-04-09 14:18:39 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3096 6845aa9c-c3df-f248-9547-742189ef8f90

--- releaseBo.sh
+++ releaseBo.sh
@@ -8,6 +8,9 @@ then
         exit -1
 fi
 
+snapshot=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+release=${snapshot/"-SNAPSHOT"/}
+
 mvn -q --batch-mode release:prepare -Darguments="-DskipTests"
  
 e="$?"
@@ -17,10 +20,8 @@ then
 	exit -1
 fi
 
-v=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
-
-echo "prepared release version $v"
+echo "prepared release version $release"
 
 mvn -q release:perform -Darguments="-DskipTests"
 
-mail -s "Bo2 $v released" `cat recipientsOne.txt`
+echo $release | mail -s "Bo2 $release released" `cat recipientsOne.txt`
