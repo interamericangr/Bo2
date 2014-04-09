59c54f5299d538723169d468993d3e084cf6e510
Dimitris Kateros <katerosd@interamerican.gr>
2014-04-09 14:06:16 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3091 6845aa9c-c3df-f248-9547-742189ef8f90

--- releaseBo.sh
+++ releaseBo.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-mvn release:prepare
+mvn clean install 
+
 e="$?"
 if [ $e -ne 0 ]
 then
-	echo "mvn build failed exiting..."
+        echo "build failed exiting..."
+        exit -1
+fi
+
+mvn --batch-mode release:prepare -Darguments="-DskipTests"
+ 
+e="$?"
+if [ $e -ne 0 ]
+then
+	echo "release preparation failed exiting..."
 	exit -1
 fi
+
 v=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+
+echo "prepared release version $v"
+
 mvn release:perform -Darguments="-DskipTests"
-mail -s "Neo BO2 $v" `cat recipientsOne.txt`<changelog.txt
+
+mail -s "Bo2 $v released" `cat recipientsOne.txt`
