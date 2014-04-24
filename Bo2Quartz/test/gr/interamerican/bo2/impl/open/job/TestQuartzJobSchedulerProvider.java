d5db2a276a5d49e019138346b837b89d6540f8e8
Dimitris Kateros <katerosd@interamerican.gr>
2014-04-24 11:00:38 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3191 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Quartz/test/gr/interamerican/bo2/impl/open/job/TestQuartzJobSchedulerProvider.java
+++ Bo2Quartz/test/gr/interamerican/bo2/impl/open/job/TestQuartzJobSchedulerProvider.java
@@ -4,16 +4,38 @@ import gr.interamerican.bo2.arch.exceptions.DataException;
 import gr.interamerican.bo2.arch.exceptions.InitializationException;
 import gr.interamerican.bo2.arch.exceptions.LogicException;
 import gr.interamerican.bo2.arch.exceptions.UnexpectedException;
+import gr.interamerican.bo2.impl.open.creation.Factory;
 import gr.interamerican.bo2.impl.open.runtime.AbstractBo2RuntimeCmd;
+import gr.interamerican.bo2.impl.open.workers.AbstractOperation;
 import gr.interamerican.bo2.quartz.QuartzJobSchedulerProviderImpl;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Integration test 
  */
 public class TestQuartzJobSchedulerProvider {
+	
+	/**
+	 * tm
+	 */
+	JobCapableTransactionManager tm;
+	
+	/**
+	 * job
+	 */
+	JobDescription job = Factory.create(JobDescription.class);
+	
+	/**
+	 * create job sample and reset TestOperation.executed
+	 */
+	@Before
+	public void before() {
+		TestOperation.executed = false;
+		job.setOperationClass(TestOperation.class);
+	}
 	
 	/**
 	 * integration test
@@ -26,14 +48,65 @@ public class TestQuartzJobSchedulerProvider {
 		new AbstractBo2RuntimeCmd() { 			
 			@Override public void work() throws LogicException, 
 			DataException, InitializationException, UnexpectedException {
-				JobCapableTransactionManager tm = (JobCapableTransactionManager) getProvider().getTransactionManager();
+				tm = (JobCapableTransactionManager) getProvider().getTransactionManager();
 				
 				Assert.assertEquals(0, tm.schedulerHandlers.size());
+				
 				JobSchedulerProvider prov = getProvider().getResource("LOCALDB", JobSchedulerProvider.class); //$NON-NLS-1$
+				prov.scheduleJob(job);
 				Assert.assertTrue(prov instanceof QuartzJobSchedulerProviderImpl);
 				Assert.assertEquals(1, tm.schedulerHandlers.size());
+				
+				prov.scheduleJob(job);
+				
+				Assert.assertFalse(TestOperation.executed);
 			}
 		}.execute();
+		
+		Assert.assertEquals(0, tm.schedulerHandlers.size());
+		Assert.assertTrue(TestOperation.executed);
+	}
+	
+	/**
+	 * integration test
+	 * @throws DataException
+	 * @throws LogicException
+	 * @throws UnexpectedException
+	 */
+	@Test
+	public void integrationTest_rollback() throws DataException, LogicException, UnexpectedException {
+		new AbstractBo2RuntimeCmd() { 			
+			@Override public void work() throws LogicException, 
+			DataException, InitializationException, UnexpectedException {
+				tm = (JobCapableTransactionManager) getProvider().getTransactionManager();
+				
+				Assert.assertEquals(0, tm.schedulerHandlers.size());
+				
+				JobSchedulerProvider prov = getProvider().getResource("LOCALDB", JobSchedulerProvider.class); //$NON-NLS-1$
+				
+				Assert.assertTrue(prov instanceof QuartzJobSchedulerProviderImpl);
+				Assert.assertEquals(1, tm.schedulerHandlers.size());
+				
+				prov.scheduleJob(job);
+				
+				Assert.assertFalse(TestOperation.executed);
+				
+				throw new RuntimeException(); //fail the uow
+			}
+		}.execute();
+		
+		Assert.assertEquals(0, tm.schedulerHandlers.size());
+		Assert.assertFalse(TestOperation.executed);
+	}
+	
+	@SuppressWarnings("javadoc")
+	public static class TestOperation extends AbstractOperation {
+		static boolean executed = false;
+
+		@Override
+		public void execute() throws LogicException, DataException {
+			executed = true;
+		}
 	}
 	
 }
