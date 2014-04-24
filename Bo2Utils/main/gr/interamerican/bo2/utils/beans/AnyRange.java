bfa529e00b5f60fafc3ff95d861de275b7e612f1
Vasileios Mylonakis <milonakisv@interamerican.gr>
2014-04-24 07:20:30 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3161 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Utils/main/gr/interamerican/bo2/utils/beans/AnyRange.java
+++ Bo2Utils/main/gr/interamerican/bo2/utils/beans/AnyRange.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.utils.beans;
@@ -22,8 +22,13 @@ package gr.interamerican.bo2.utils.beans;
  * 
  * @param <T> Type of element.
  */
-public final class AnyRange<T extends Comparable<? super T>> 
+public final class AnyRange<T extends Comparable<? super T>>
 extends Range<T> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Creates a new Range object.
@@ -32,7 +37,7 @@ extends Range<T> {
 	public AnyRange() {
 		super(null, null);
 	}
-	
+
 	@Override
 	public boolean overlapsWith(Range<T> range) {
 		return true;
@@ -44,34 +49,34 @@ extends Range<T> {
 	}
 
 	@Override
-	public int compareTo(Range<T> other) {		
+	public int compareTo(Range<T> other) {
 		if (other instanceof AnyRange) {
 			return 0;
 		}
 		return 1;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		return (obj instanceof AnyRange);
 	}
 
-    @Override
-    public String toString() {    
-    	return "..."; //$NON-NLS-1$
-    }
-    
-    @Override
-    public int hashCode() {    	
-    	return 891584;    	
-    }
-    
-    @Override
-    public void setLeft(T left) {/* empty */}
-    
-    @Override
-    public void setRight(T right) {/* empty */}
-    
-    
-    
+	@Override
+	public String toString() {
+		return "..."; //$NON-NLS-1$
+	}
+
+	@Override
+	public int hashCode() {
+		return 891584;
+	}
+
+	@Override
+	public void setLeft(T left) {/* empty */}
+
+	@Override
+	public void setRight(T right) {/* empty */}
+
+
+
 }
