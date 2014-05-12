6daeeed401087329f81c2f3445155a05b16f7325
Dimitris Kateros <katerosd@interamerican.gr>
2014-05-12 12:10:19 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3283 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplJee/test/gr/interamerican/bo2/impl/open/jee/jaxws/TestHostPortUrlModifier.java
+++ Bo2ImplJee/test/gr/interamerican/bo2/impl/open/jee/jaxws/TestHostPortUrlModifier.java
@@ -20,16 +20,23 @@ public class TestHostPortUrlModifier {
 	@Test
 	public void testExecute() throws MalformedURLException {
 		HostPortUrlModifier mod = new HostPortUrlModifier("127.0.0.1:8080");
-		
 		Assert.assertEquals("127.0.0.1", mod.host);
 		Assert.assertEquals(8080, mod.port);
 		
 		URL u = new URL("http://host/file");
-		
 		String expected = "http://127.0.0.1:8080/file";
 		String actual = mod.execute(u).toExternalForm();
-		
 		Assert.assertEquals(expected, actual);
+		
+		//no port
+		mod = new HostPortUrlModifier("127.0.0.1");
+		Assert.assertEquals("127.0.0.1", mod.host);
+		Assert.assertEquals(80, mod.port);
+		
+		expected = "http://127.0.0.1:80/file";
+		actual = mod.execute(u).toExternalForm();
+		Assert.assertEquals(expected, actual);
+		
 	}
 
 }
