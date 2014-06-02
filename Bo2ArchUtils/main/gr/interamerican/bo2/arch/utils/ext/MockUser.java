a4f2958ced104ff27dc57f0a80e104750ba37a4a
noname <noname@interamerican.gr>
2014-06-02 13:03:55 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3413 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/ext/MockUser.java
+++ Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/ext/MockUser.java
@@ -1,7 +1,9 @@
 package gr.interamerican.bo2.arch.utils.ext;
 
 import gr.interamerican.bo2.arch.ext.User;
+import gr.interamerican.bo2.utils.StringConstants;
 import gr.interamerican.bo2.utils.StringUtils;
+import gr.interamerican.bo2.utils.TokenUtils;
 
 /**
  * Mock user.<br>
@@ -81,10 +83,29 @@ implements User<A>{
 		return authorized;
 	}
 
+	/**
+	 * include pml authorization.
+	 */
+	@SuppressWarnings("nls")
 	@Override
 	public String getAuthorizationToken(A authorizationId) {
+		String[] auth = TokenUtils.splitTrim((String) authorizationId, StringConstants.MINUS);
+		if (((String) authorizationId).length() < 3) {
+			String msg = "Invalid authorization id: " + authorizationId; //$NON-NLS-1$
+			throw new RuntimeException(msg);
+		}
+		String branchCd = auth[1];
+		String authCd = auth[2];
+		if (authCd.equals("419")) {
+			if (branchCd.equals("02")) {
+				authToken = "69";
+			} else if (branchCd.equals("03")) {
+				authToken = "81";
+			}
+		}
 		return authToken;
 	}
+
 
 	@Override
 	public String authorizationDescription(A authorizationId) {
