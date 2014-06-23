10719d1c0660fea9a69b31cfae04d0272789e8b6
Dimitris Kateros <katerosd@interamerican.gr>
2014-06-23 11:51:52 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3448 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/jdbc/JdbcPropertiesLoaderQuestion.java
@@ -0,0 +1,89 @@
+package gr.interamerican.bo2.impl.open.jdbc;
+
+import gr.interamerican.bo2.arch.exceptions.DataException;
+import gr.interamerican.bo2.arch.exceptions.LogicException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Abstract implementation of a Question that loads a Properties instance from
+ * a table. Each row is a single property. The first column of the table is the 
+ * key and the second the value. A 3rd column may, optionally, indicate if the 
+ * value is obfuscated. The concrete decoding mechanism is left to the implementor.
+ */
+public abstract class JdbcPropertiesLoaderQuestion 
+extends JdbcSingleStatementQuestion<Properties>{
+	
+	/**
+	 * Answer
+	 */
+	private Properties answer;
+	
+	@Override
+	public Properties getAnswer() {
+		return answer;
+	}
+		
+	@Override
+	protected void work() throws DataException, LogicException {
+		answer = new Properties();
+		String stmt = sql();
+		try {			
+			Object[] parms = this.getParamsFromNamedParams();
+			ResultSet rs = executePreparedQuery(stmt,parms);
+			while(rs.next()) {
+				String key = rs.getString(1);
+				String value = rs.getString(2);
+				
+				if(isCurrentRowObfuscated(rs)) {
+					value = decode(value);
+				}
+				
+				Object prev = answer.setProperty(key, value);
+				if(prev!=null) {
+					handleDuplicateKey(key);
+				}
+			}
+		} catch (SQLException e) {
+			throw new DataException(e);
+		}
+	}
+	
+	/**
+	 * Handles a duplicate key value. The default implementation throws
+	 * a DataException.
+	 * 
+	 * @param dulpicateKey
+	 * @throws DataException
+	 */
+	protected void handleDuplicateKey(Object dulpicateKey) throws DataException {
+		throw new DataException("Duplicate value " + dulpicateKey); //$NON-NLS-1$
+	}
+	
+	/**
+	 * Hook for decoding an obfuscated value.
+	 * <br/>
+	 * Users of this class may override this to provide a desirable implementation.
+	 * 
+	 * @param obfuscatedValue
+	 * @return The decoded value.
+	 */
+	protected String decode(String obfuscatedValue) {
+		return obfuscatedValue;
+	}
+	
+	/**
+	 * @param rs
+	 * @return Returns true if the current row is obfuscated.
+	 */
+	boolean isCurrentRowObfuscated(ResultSet rs) {
+		try {
+			return rs.getBoolean(3);
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+}
---
+++ Bo2ImplOpen/test/gr/interamerican/bo2/impl/open/jdbc/TestJdbcPropertiesLoaderQuestion.java
@@ -0,0 +1,64 @@
+package gr.interamerican.bo2.impl.open.jdbc;
+
+import gr.interamerican.bo2.arch.exceptions.DataException;
+import gr.interamerican.bo2.arch.exceptions.InitializationException;
+import gr.interamerican.bo2.arch.exceptions.LogicException;
+import gr.interamerican.bo2.arch.exceptions.UnexpectedException;
+import gr.interamerican.bo2.impl.open.annotations.ManagerName;
+import gr.interamerican.bo2.impl.open.runtime.AbstractBo2RuntimeCmd;
+
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JdbcPropertiesLoaderQuestion test
+ */
+public class TestJdbcPropertiesLoaderQuestion {
+	
+	/**
+	 * test subject
+	 */
+	JdbcPropertiesLoaderQuestion impl = new JdbcPropertiesLoaderQuestionImpl();
+	
+	/**
+	 * test ask
+	 * 
+	 * @throws DataException
+	 * @throws LogicException
+	 * @throws UnexpectedException
+	 */
+	@Test
+	public void testAsk() throws DataException, LogicException, UnexpectedException {
+		new AbstractBo2RuntimeCmd() {
+			
+			@SuppressWarnings("nls")
+			@Override
+			public void work() throws LogicException, DataException, InitializationException, UnexpectedException {
+				open(impl);
+				impl.ask();
+				Properties p = impl.getAnswer();
+				Assert.assertFalse(p.isEmpty());
+				Assert.assertEquals("1", p.getProperty("1"));
+			}
+		}.execute();
+	}
+	
+	/**
+	 * implementation to test
+	 */
+	@ManagerName("LOCALDB")
+	private class JdbcPropertiesLoaderQuestionImpl extends JdbcPropertiesLoaderQuestion {	
+		/**
+		 * sgl.
+		 */		
+		@Sql private String sql = "select '1','1' from X__X.users"; //$NON-NLS-1$
+		
+		@Override
+		protected void handleDuplicateKey(Object dulpicateKey) throws DataException {
+			/* empty */
+		}
+	}
+
+}
