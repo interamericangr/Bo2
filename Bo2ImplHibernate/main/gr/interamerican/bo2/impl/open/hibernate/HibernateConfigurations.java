bc0466214a4638fe3ef5dd58ddcce06c66a12946
noname <noname@interamerican.gr>
2014-06-26 13:58:10 +0000

    changed logging of missing hibernate mapping from error to warning
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3468 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplHibernate/main/gr/interamerican/bo2/impl/open/hibernate/HibernateConfigurations.java
+++ Bo2ImplHibernate/main/gr/interamerican/bo2/impl/open/hibernate/HibernateConfigurations.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.impl.open.hibernate;
@@ -36,28 +36,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class manages all hibernate configurations and their SessionFactory 
+ * This class manages all hibernate configurations and their SessionFactory
  * objects.
  */
 public class HibernateConfigurations {
-	
+
 	/**
 	 * Logger.
 	 */
 	private static Logger LOGGER = LoggerFactory.getLogger(HibernateConfigurations.class);
-	
+
 	/**
 	 * Schema property on a hibernate config.
 	 */
 	static final String SCHEMA_PROPERTY = "hibernate.default_schema"; //$NON-NLS-1$
-	
+
 	/**
 	 * Static map that stores all session factories, mapped to the path
 	 * of their configuration files.
 	 */
-	static Map<String, SessionFactory> sessionFactories = 
-		new HashMap<String, SessionFactory>();
-	
+	static Map<String, SessionFactory> sessionFactories =
+			new HashMap<String, SessionFactory>();
+
 	/**
 	 * Gets the session factory that corresponds to the specified configuration
 	 * file.
@@ -66,7 +66,7 @@ public class HibernateConfigurations {
 	 *        Path to the configuration resource file.
 	 * @param dbSchema
 	 *        Db schema.
-	 * @param sessionInterceptor 
+	 * @param sessionInterceptor
 	 *        Hibernate session interceptor.
 	 * @param hibernateMappingsPath
 	 *        Path to file that lists files indexing hbm files this session factory
@@ -78,8 +78,8 @@ public class HibernateConfigurations {
 	 *         If the creation of the session factory fails.
 	 */
 	public static synchronized SessionFactory getSessionFactory(
-		String pathToCfg, String dbSchema, String sessionInterceptor, String hibernateMappingsPath) 
-	throws InitializationException {
+			String pathToCfg, String dbSchema, String sessionInterceptor, String hibernateMappingsPath)
+					throws InitializationException {
 		SessionFactory sessionFactory = sessionFactories.get(key(pathToCfg, dbSchema));
 		if (sessionFactory==null) {
 			sessionFactory = createSessionFactory(pathToCfg, dbSchema, sessionInterceptor, hibernateMappingsPath);
@@ -87,7 +87,7 @@ public class HibernateConfigurations {
 		}
 		return sessionFactory;
 	}
-	
+
 	/**
 	 * Closes opened session factories. A web application should call this before
 	 * unloading or stopping.
@@ -98,7 +98,7 @@ public class HibernateConfigurations {
 		}
 		sessionFactories.clear();
 	}
-	
+
 	/**
 	 * Creates a SessionFactory.
 	 * 
@@ -111,33 +111,33 @@ public class HibernateConfigurations {
 	 * @param hibernateMappingsPath
 	 *        Path to file that lists files indexing hbm files this session factory
 	 *        should be configured with
-	 *        
+	 * 
 	 * @return Returns the session factory.
 	 * 
-	 * @throws InitializationException 
+	 * @throws InitializationException
 	 *         If the creation of the SessionFactory fails.
 	 */
 	@SuppressWarnings("nls")
-	static SessionFactory createSessionFactory(String pathToCfg, String dbSchema, String sessionInterceptor, String hibernateMappingsPath) 
-	throws InitializationException {
+	static SessionFactory createSessionFactory(String pathToCfg, String dbSchema, String sessionInterceptor, String hibernateMappingsPath)
+			throws InitializationException {
 		try {
 			Configuration conf = new Configuration();
-			
+
 			Interceptor interceptor = getInterceptor(sessionInterceptor);
 			if(interceptor!=null) {
 				conf.setInterceptor(interceptor);
 			}
-			
+
 			conf.setProperty(SCHEMA_PROPERTY, dbSchema);
-			
+
 			List<String> hbms = getHibernateMappingsIfAvailable(hibernateMappingsPath);
 			for(String entityMapping : hbms) {
 				LOGGER.debug("Adding " + entityMapping + " to the session factory configuration.");
 				conf.addResource(entityMapping);
 			}
-			
+
 			conf.configure(pathToCfg);
-			
+
 			conf.getEntityTuplizerFactory().registerDefaultTuplizerClass(EntityMode.POJO, Bo2PojoEntityTuplizer.class);
 			SessionFactory sessionFactory = conf.buildSessionFactory();
 			((SessionFactoryImpl) sessionFactory).registerEntityNameResolver(Bo2EntityNameResolver.INSTANCE, EntityMode.POJO);
@@ -147,7 +147,7 @@ public class HibernateConfigurations {
 			throw new InitializationException(e);
 		}
 	}
-	
+
 	/**
 	 * Get the interceptor instance if it is configured.
 	 * @param sessionInterceptor
@@ -159,7 +159,7 @@ public class HibernateConfigurations {
 		}
 		return ReflectionUtils.newInstance(sessionInterceptor);
 	}
-	
+
 	/**
 	 * @param pathToCfg
 	 * @param dbSchema
@@ -168,10 +168,10 @@ public class HibernateConfigurations {
 	private static String key(String pathToCfg, String dbSchema) {
 		return pathToCfg + StringConstants.UNDERSCORE + dbSchema;
 	}
-	
+
 	/**
 	 * Extracts a list of hibernate mapping files given a resource path.
-	 * The resource path points to a file. The file is plain text and 
+	 * The resource path points to a file. The file is plain text and
 	 * each line contains one resource path that points to an hbm.xml file.
 	 * If an indexed hdm.xml file does not exist on the classpath an error
 	 * is logged, but no exception is thrown. This is meant to facilitate
@@ -180,52 +180,52 @@ public class HibernateConfigurations {
 	 * 
 	 * @param managerHbmIndex
 	 *        Resource path. Null tolerant.
-	 *        
+	 * 
 	 * @return Indexed hbm files resource paths.
 	 */
 	@SuppressWarnings("nls")
 	static List<String> getHibernateMappingsIfAvailable(String managerHbmIndex) {
 		List<String> hbms = new ArrayList<String>();
-		
+
 		if(StringUtils.isNullOrBlank(managerHbmIndex)) {
 			return hbms;
 		}
-		
+
 		String[] hbmPaths = null;
 		try {
 			hbmPaths = StreamUtils.readResourceFile(managerHbmIndex, true, true);
 		} catch (IOException ioe) {
 			throw new RuntimeException(ioe);
 		}
-		
+
 		if(hbmPaths==null) {
 			String msg = "Non existant deployment hbm.xml index: " + managerHbmIndex;
 			throw new RuntimeException(msg);
 		}
-		
+
 		for(String hbmPath : hbmPaths) {
 			String[] hbmContent = null;
-			
+
 			try {
 				hbmContent = StreamUtils.readResourceFile(StringConstants.SLASH + hbmPath.trim(), true, true);
 			} catch (IOException ioe) {
 				throw new RuntimeException(ioe);
 			}
-			
+
 			if(hbmContent==null) {
 				String msg = StringUtils.concat(
 						"Non existant hibernate mappings file: ",
 						hbmPath,
 						". This is acceptable for unit tests, but FATAL in every other case and should be investigated.");
-				LOGGER.error(msg);
+				LOGGER.warn(msg);
 				continue;
 			}
-			
+
 			hbms.add(hbmPath);
 
 		}
-		
+
 		return hbms;
 	}
-	
+
 }
