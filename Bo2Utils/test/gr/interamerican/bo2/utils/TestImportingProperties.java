a015c8a49b36f362b5bebceb8a5f62b9271a000d
noname <noname@interamerican.gr>
2014-07-15 09:05:25 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3534 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Utils/test/gr/interamerican/bo2/utils/TestImportingProperties.java
+++ Bo2Utils/test/gr/interamerican/bo2/utils/TestImportingProperties.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.utils;
@@ -23,16 +23,18 @@ import org.junit.Test;
  * Unit tests for {@link ImportingProperties}.
  */
 public class TestImportingProperties {
-	
+
 	/**
 	 * Tests load.
 	 */
 	@Test
 	public void testLoad() {
-		String path = "/gr/interamerican/bo2/utils/improps.properties"; //$NON-NLS-1$
+		String path = "gr/interamerican/bo2/utils/improps.properties"; //$NON-NLS-1$
 		Properties ext = new ImportingProperties();
 		try {
-			InputStream is = this.getClass().getResourceAsStream(path);
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			InputStream is = cl.getResourceAsStream(path);
+			Assert.assertNotNull(is);
 			ext.load(is);
 		} catch (IOException e) {
 			throw new RuntimeException("Failed to load imported Properties file: " + path); //$NON-NLS-1$
