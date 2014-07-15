9bcf66a58272323925c3e6a174583c275e87a5d4
noname <noname@interamerican.gr>
2014-07-15 09:05:13 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3533 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Utils/main/gr/interamerican/bo2/utils/StreamUtils.java
+++ Bo2Utils/main/gr/interamerican/bo2/utils/StreamUtils.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
  * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.utils;
@@ -30,27 +30,27 @@ import java.util.ArrayList;
  * Utilities about streams.
  */
 public class StreamUtils {
-	
-	
-	
+
+
+
 	/**
 	 * Reads a text file from a resource file in the local classpath
 	 * and returns an array with the lines of the file.
 	 * 
 	 * @see #readResourceFile(String, boolean, boolean)
 	 * 
-	 * @param path Path to the file. 
+	 * @param path Path to the file.
 	 * 
 	 * @return Returns an array with names of mappings files.
-	 *         If the resource file is not found, then returns null. 
-	 * @throws IOException 
+	 *         If the resource file is not found, then returns null.
+	 * @throws IOException
 	 * @throws NullPointerException if the arg is null
 	 */
-	public static String[] readResourceFile(String path) 
-	throws IOException {		
+	public static String[] readResourceFile(String path)
+			throws IOException {
 		return readResourceFile(path, false, false);
 	}
-	
+
 	/**
 	 * Reads a text file from a resource file in the local classpath
 	 * and returns an array with the lines of the file.
@@ -60,22 +60,22 @@ public class StreamUtils {
 	 * The file is assumed to be encoded with the default Bo2 deployment
 	 * <code>resourceFileEncoding</code> property.
 	 * 
-	 * @param path 
-	 *        Path to the file. 
+	 * @param path
+	 *        Path to the file.
 	 * @param excludeEmptyLines
-	 *        If set to true, empty lines will be excluded. 
-	 * @param excludeSharps 
+	 *        If set to true, empty lines will be excluded.
+	 * @param excludeSharps
 	 *        If true, anything that follows a '#' in a line is ignored
 	 * 
 	 * @return Returns an array with names of mappings files.
 	 *         If the resource file is not found, then returns null.
-	 *          
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 * @throws NullPointerException if the arg is null
 	 */
-	public static String[] readResourceFile(String path, boolean excludeEmptyLines, boolean excludeSharps) 
-	throws IOException {		
-		InputStream stream = StreamUtils.class.getResourceAsStream(path);
+	public static String[] readResourceFile(String path, boolean excludeEmptyLines, boolean excludeSharps)
+			throws IOException {
+		InputStream stream = getResourceStream0(path);
 		if(stream==null){
 			return null;
 		}
@@ -83,23 +83,23 @@ public class StreamUtils {
 		BufferedReader reader = new BufferedReader(insr);
 		return StreamUtils.consumeBufferedReader(reader, excludeEmptyLines, excludeSharps);
 	}
-	
+
 	/**
 	 * Reads a text file from the filesystem and returns an array with the lines of the file.
 	 * 
 	 * @see #readFile(String, boolean, boolean)
 	 * 
-	 * @param path Path to the file. 
+	 * @param path Path to the file.
 	 * 
 	 * @return Returns an array with names of mappings files.
-	 *         If the resource file is not found, then returns null. 
-	 * @throws IOException 
+	 *         If the resource file is not found, then returns null.
+	 * @throws IOException
 	 */
-	public static String[] readFile(String path) 
-	throws IOException {
+	public static String[] readFile(String path)
+			throws IOException {
 		return readFile(path, false, false);
 	}
-	
+
 	/**
 	 * Reads a text file from the filesystem and returns an array with the lines of the file.
 	 * <br/>
@@ -107,19 +107,19 @@ public class StreamUtils {
 	 * 
 	 * @see Bo2UtilsEnvironment
 	 * 
-	 * @param path 
-	 *        Path to the file. 
-	 * @param excludeEmptyLines 
-	 *        If set to true, empty lines will be excluded. 
-	 * @param excludeSharps 
+	 * @param path
+	 *        Path to the file.
+	 * @param excludeEmptyLines
+	 *        If set to true, empty lines will be excluded.
+	 * @param excludeSharps
 	 *        If true, anything that follows a '#' in a line is ignored
-	 *        
+	 * 
 	 * @return Returns an array with names of mappings files.
-	 *         If the resource file is not found, then returns null. 
-	 * @throws IOException 
+	 *         If the resource file is not found, then returns null.
+	 * @throws IOException
 	 */
-	public static String[] readFile(String path, boolean excludeEmptyLines, boolean excludeSharps) 
-	throws IOException {
+	public static String[] readFile(String path, boolean excludeEmptyLines, boolean excludeSharps)
+			throws IOException {
 		try {
 			File file = new File(path);
 			InputStream stream = new FileInputStream(file);
@@ -130,7 +130,7 @@ public class StreamUtils {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Gets a String from a resource file.
 	 * 
@@ -140,10 +140,10 @@ public class StreamUtils {
 	 * 
 	 * @param path
 	 *        Path to the resource file.
-	 *        
+	 * 
 	 * @return Returns the string in the resource file.
 	 */
-	public static String getStringFromResourceFile(String path) {				
+	public static String getStringFromResourceFile(String path) {
 		String[] rows;
 		try {
 			rows = readResourceFile(path);
@@ -157,12 +157,12 @@ public class StreamUtils {
 		}
 		return StringUtils.array2String(rows, StringConstants.SPACE);
 	}
-	
+
 	/**
 	 * Gets the raw contents of a file as bytes.
 	 * 
 	 * @param file
-	 * @return Returns a byte array that contains the file contents. 
+	 * @return Returns a byte array that contains the file contents.
 	 */
 	public static byte[] getFileContents(File file) {
 		FileInputStream stream = null;
@@ -172,7 +172,7 @@ public class StreamUtils {
 				String msg = "The file is too large"; //$NON-NLS-1$
 				throw new RuntimeException(msg);
 			}
-			int iSize = Long.valueOf(size).intValue();		
+			int iSize = Long.valueOf(size).intValue();
 			stream = new FileInputStream(file);
 			byte[] buffer = new byte[iSize];
 			stream.read(buffer);
@@ -191,7 +191,7 @@ public class StreamUtils {
 			}
 		}
 	}
-	
+
 	/**
 	 * Gets the contents of a file.
 	 * 
@@ -201,12 +201,12 @@ public class StreamUtils {
 	 *        If this is true, then the path must point to a resource file
 	 *        in the class path, otherwise the path must point to a file.
 	 * 
-	 * @return Returns a byte array that contains the file contents. 
+	 * @return Returns a byte array that contains the file contents.
 	 */
-	public static byte[] getFileContents(String path, boolean inClassPath) {	
+	public static byte[] getFileContents(String path, boolean inClassPath) {
 		return getFileContents(getFile(path, inClassPath));
 	}
-	
+
 	/**
 	 * Creates a File object for a path.
 	 * 
@@ -218,13 +218,13 @@ public class StreamUtils {
 	 * 
 	 * @return Returns a File for the specified path.
 	 */
-	public static File getFile(String path, boolean inClassPath) {	
+	public static File getFile(String path, boolean inClassPath) {
 		try {
 			if (inClassPath) {
 				URL url = StreamUtils.class.getResource(path);
 				URI uri;
 				uri = url.toURI();
-				return new File(uri);	
+				return new File(uri);
 			} else {
 				return new File(path);
 			}
@@ -232,15 +232,15 @@ public class StreamUtils {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	/**
 	 * File to byte[]
-	 * @param in 
+	 * @param in
 	 * @return byte[]
 	 * @throws IOException
 	 */
-	public static byte[] inputStreamToByteArray(InputStream in) 
-	throws IOException {		
+	public static byte[] inputStreamToByteArray(InputStream in)
+			throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		byte[] buffer = new byte[65536];
 		int read = 0;
@@ -252,69 +252,92 @@ public class StreamUtils {
 		out.close();
 		return bytes;
 	}
-	
+
 	/**
 	 * �������� ��� ��� file ��� ������� ��� byte[].
-	 * @param arr 
+	 * @param arr
 	 * @param file �� file ��� �� �����������.
-	 * @throws IOException 
+	 * @throws IOException
 	 * 
 	 */
 	public static void saveToFile(byte[] arr, File file) throws IOException{
-	  	FileOutputStream fos = new FileOutputStream(file);
-	  	fos.write(arr);
-        fos.flush();
-        fos.close();		
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(arr);
+		fos.flush();
+		fos.close();
 	}
-	
+
+
+	/**
+	 * @param path
+	 * @return the inputstream.
+	 * @throws IOException
+	 */
+	static InputStream getResourceStream0(String path) throws IOException {
+		if (StringUtils.isNullOrBlank(path)) {
+			throw new IOException("Path is null"); //$NON-NLS-1$
+		}
+		if (path.startsWith(StringConstants.SLASH)) {
+			InputStream stream = StreamUtils.class.getResourceAsStream(path);
+			if (stream == null) {
+				stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path.substring(1));
+				return stream;
+			}
+		}
+		InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path.substring(1));
+		if (stream == null) {
+			stream = StreamUtils.class.getResourceAsStream(StringConstants.SLASH + path);
+		}
+		return stream;
+	}
+
 	/**
 	 * Opens a resource file input stream. <br/>
 	 * 
 	 * @param path
 	 *        path to the resource stream.
-	 *        
+	 * 
 	 * @return Returns the stream.
 	 * 
 	 * @throws IOException
 	 *         If the resource path is not found.
-	 *   
+	 * 
 	 */
-	public static InputStream getResourceStream(String path) 
-	throws IOException {
-		InputStream stream = StreamUtils.class.getResourceAsStream(path);
-		if (stream == null) {			
+	public static InputStream getResourceStream(String path) throws IOException {
+		InputStream stream = getResourceStream0(path);
+		if (stream == null) {
 			@SuppressWarnings("nls")
 			String msg = "Resource " + path + " not found";
-			throw new IOException(msg);			
+			throw new IOException(msg);
 		}
 		return stream;
 	}
-	
+
 	/**
 	 * Opens an input stream from a filesystem file. <br/>
 	 * 
 	 * @param path
 	 *        path to the resource stream.
-	 *        
+	 * 
 	 * @return Returns the stream.
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static InputStream getFileStream(String path) 
-	throws IOException {
+	public static InputStream getFileStream(String path)
+			throws IOException {
 		FileInputStream stream = new FileInputStream(path);
 		return stream;
 	}
-	
+
 	/**
 	 * Reads a text file from a path.
 	 * 
-	 * @param path path. 
+	 * @param path path.
 	 * 
 	 * @return Returns a string with the contents of the file.
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static String readTextFile(String path) 
-	throws IOException {
+	public static String readTextFile(String path)
+			throws IOException {
 		InputStream is = getFileStream(path);
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader reader = new BufferedReader(isr);
@@ -328,21 +351,21 @@ public class StreamUtils {
 		} while (line!=null);
 		return text;
 	}
-	
+
 	/**
 	 * Reads a text file from a {@link BufferedReader}
 	 * and returns an array with the lines of the file.
 	 * The reader is close after use.
 	 * 
-	 * @param reader BufferedReader. 
+	 * @param reader BufferedReader.
 	 * @param excludeEmptyLines If true, empty lines will be excluded.
 	 * @param excludeSharps If true, anything that follows a '#' in a line is ignored
 	 * 
 	 * @return Returns an array with names of mappings files.
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	static String[] consumeBufferedReader(BufferedReader reader, boolean excludeEmptyLines, boolean excludeSharps) 
-	throws IOException {
+	static String[] consumeBufferedReader(BufferedReader reader, boolean excludeEmptyLines, boolean excludeSharps)
+			throws IOException {
 		ArrayList<String> lines = new ArrayList<String>();
 		boolean eof = false;
 		while (!eof) {
@@ -355,16 +378,16 @@ public class StreamUtils {
 						line = line.substring(0, index);
 					}
 				}
-				
+
 				if (excludeEmptyLines) {
 					if (line.length()>0) {
 						lines.add(line);
 					}
-				} 
+				}
 				else {
 					lines.add(line);
 				}
-				
+
 			} else {
 				eof = true;
 			}
