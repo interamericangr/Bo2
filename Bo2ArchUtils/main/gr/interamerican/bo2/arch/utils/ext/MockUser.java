d20d2dc777c19c7df68374548cca14ce7e5c4b54
Dimitris Kateros <katerosd@interamerican.gr>
2014-07-16 11:42:22 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3539 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/ext/MockUser.java
+++ Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/ext/MockUser.java
@@ -1,9 +1,7 @@
 package gr.interamerican.bo2.arch.utils.ext;
 
 import gr.interamerican.bo2.arch.ext.User;
-import gr.interamerican.bo2.utils.StringConstants;
 import gr.interamerican.bo2.utils.StringUtils;
-import gr.interamerican.bo2.utils.TokenUtils;
 
 /**
  * Mock user.<br>
@@ -83,33 +81,6 @@ implements User<A>{
 		return authorized;
 	}
 
-	/**
-	 * include pml authorization.
-	 * 
-	 * @deprecated TODO Will just return authToken as soon OnE goes live for motor
-	 */
-	@SuppressWarnings("nls")
-	@Override
-	@Deprecated
-	public String getAuthorizationToken(A authorizationId) {
-		String[] auth = TokenUtils.splitTrim((String) authorizationId, StringConstants.MINUS);
-		if (((String) authorizationId).length() < 3) {
-			String msg = "Invalid authorization id: " + authorizationId; //$NON-NLS-1$
-			throw new RuntimeException(msg);
-		}
-		String branchCd = auth[1];
-		String authCd = auth[2];
-		if (authCd.equals("419")) {
-			if (branchCd.equals("02")) {
-				authToken = "69";
-			} else if (branchCd.equals("03")) {
-				authToken = "91";
-			}
-		}
-		return authToken;
-	}
-
-
 	@Override
 	public String authorizationDescription(A authorizationId) {
 		return StringUtils.toString(authorizationId);
@@ -133,6 +104,10 @@ implements User<A>{
 	@Override
 	public String getEmailAddress() {
 		return "mock@email.com"; //$NON-NLS-1$
+	}
+
+	public String getAuthorizationToken(A authorizationId) {
+		return authToken;
 	}
 
 }
