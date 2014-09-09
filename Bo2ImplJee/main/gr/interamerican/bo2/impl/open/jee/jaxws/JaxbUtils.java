044f9c3276e6c05f0dc2ccf7365414704c47a928
Dimitris Kateros <katerosd@interamerican.gr>
2014-09-09 11:40:40 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3590 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2ImplJee/main/gr/interamerican/bo2/impl/open/jee/jaxws/JaxbUtils.java
@@ -0,0 +1,42 @@
+package gr.interamerican.bo2.impl.open.jee.jaxws;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * Utilities for JAXB
+ */
+public class JaxbUtils {
+
+	/**
+	 * Unmarshal XML data from the specified InputStream and return the 
+     * resulting bean.
+     * <br/>
+     * This assumes that all necessary classes associated to the bean are
+	 * directly/indirectly referenced statically from its class.
+     * 
+	 * @param beanType
+	 * @param xml
+	 * 
+	 * @see JAXBContext
+	 * @see Unmarshaller
+	 * 
+	 * @return Bean instance
+	 */
+	public static <T> T unmarshal(Class<T> beanType, InputStream xml) {
+		try {
+			JAXBContext jc = JAXBContext.newInstance(beanType);
+			Unmarshaller u = jc.createUnmarshaller();
+			
+			@SuppressWarnings("unchecked")
+			T bean = (T) u.unmarshal(xml);
+			return bean;
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
