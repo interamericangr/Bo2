a00b75b5d748f24921d5d6d14056801734364f09
Dimitris Kateros <katerosd@interamerican.gr>
2014-09-12 11:24:09 +0000

    test
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3601 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2ImplOpen/test/gr/interamerican/bo2/impl/open/Qt.java
@@ -0,0 +1,35 @@
+package gr.interamerican.bo2.impl.open;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import gr.interamerican.bo2.impl.open.creation.Factory;
+import gr.interamerican.bo2.test.def.posamples.Invoice;
+import gr.interamerican.bo2.test.impl.posamples.InvoiceImpl;
+
+/**
+ * 
+ */
+public class Qt {
+	
+	@Test
+	public void test1() {
+		System.out.println("#@#@");
+		
+		
+		Invoice i = Factory.create(Invoice.class);
+		
+		Class<?> enhanced = i.getClass();
+		System.out.println(enhanced.getName());
+		
+		ClassLoader enhancedCl = enhanced.getClassLoader();
+		
+		ClassLoader standardCl = InvoiceImpl.class.getClassLoader();
+		
+		System.out.println(enhancedCl==standardCl);
+		
+		i.setInvoiceDate(new Date());
+	}
+
+}
