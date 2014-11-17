63a289ae187a5355b709a0aa22f37bf2fd4a9e40
Dimitris Kateros <katerosd@interamerican.gr>
2014-11-17 10:32:53 +0000

    test components that need an active request cycle when instantiated
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3688 6845aa9c-c3df-f248-9547-742189ef8f90

--- WicketBo2/main/gr/interamerican/wicket/bo2/test/Bo2WicketTest.java
+++ WicketBo2/main/gr/interamerican/wicket/bo2/test/Bo2WicketTest.java
@@ -12,9 +12,14 @@
  ******************************************************************************/
 package gr.interamerican.wicket.bo2.test;
 
+import gr.interamerican.bo2.arch.exceptions.CouldNotRollbackException;
+import gr.interamerican.wicket.bo2.protocol.http.Bo2WicketRequestCycle;
 import gr.interamerican.wicket.test.WicketTest;
 
+import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.junit.Assert;
 
 
 /**
@@ -42,6 +47,36 @@ public class Bo2WicketTest extends WicketTest{
 	 */
 	protected Bo2WicketTest(WebApplication application) {
 		super(application);
+	}
+	
+	/**
+	 * Tests the creation of a component that requires resources (e.g. executes a Worker) 
+	 * on its constructor and that a Page can be started with it.
+	 * To use this, override {@link #initializeComponent()} and this method (add @Test too!)
+	 * 
+	 * This is hacky, but there is no other way to test such components. 
+	 */
+	public void testComponentCreationInTestPage() {
+		RequestCycle cycle = RequestCycle.get();
+		Bo2WicketRequestCycle.beginRequest(cycle);
+		
+		Page testPage = null;
+		try {
+			testPage = getTestPage();
+			Bo2WicketRequestCycle.provider().getTransactionManager().commit();
+		} catch (Exception e) {
+			try {
+				Bo2WicketRequestCycle.provider().getTransactionManager().rollback();
+			} catch (CouldNotRollbackException cnre) {
+				//do nothing
+			}
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		} finally {
+			Bo2WicketRequestCycle.endRequest(cycle);
+		}
+		
+		tester.startPage(testPage);
 	}
 
 }
