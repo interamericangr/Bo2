5a47c69e60de10e6af8c333753ac28c571025e1a
Dimitris Kateros <katerosd@interamerican.gr>
2014-11-18 11:48:35 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3690 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/MockUtils.java
+++ Bo2ArchUtils/main/gr/interamerican/bo2/arch/utils/MockUtils.java
@@ -12,10 +12,14 @@
  ******************************************************************************/
 package gr.interamerican.bo2.arch.utils;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import gr.interamerican.bo2.arch.EntitiesQuery;
+import gr.interamerican.bo2.arch.PersistenceWorker;
+import gr.interamerican.bo2.arch.PersistentObject;
 import gr.interamerican.bo2.arch.exceptions.DataAccessException;
+import gr.interamerican.bo2.arch.exceptions.DataException;
 
 import java.util.Iterator;
 import java.util.List;
@@ -36,20 +40,23 @@ public class MockUtils {
 	 * @param clazz
 	 * @param iterator
 	 * @return Mock.
-	 * @throws DataAccessException
 	 */
-	public static <P, T extends EntitiesQuery<P>> T mockEntitiesQuery(Class<T> clazz, final Iterator<P> iterator) throws DataAccessException {
+	public static <P, T extends EntitiesQuery<P>> T mockEntitiesQuery(Class<T> clazz, final Iterator<P> iterator) {
 		T result = mock(clazz);
-		when(result.next()).thenAnswer(new Answer<Boolean>() {
-			public Boolean answer(InvocationOnMock invocation) throws Throwable {
-				return iterator.hasNext();
-			}
-		});
-		when(result.getEntity()).thenAnswer(new Answer<P>() {
-			public P answer(InvocationOnMock invocation) throws Throwable {
-				return iterator.next();
-			}
-		});
+		try {
+			when(result.next()).thenAnswer(new Answer<Boolean>() {
+				public Boolean answer(InvocationOnMock invocation) throws Throwable {
+					return iterator.hasNext();
+				}
+			});
+			when(result.getEntity()).thenAnswer(new Answer<P>() {
+				public P answer(InvocationOnMock invocation) throws Throwable {
+					return iterator.next();
+				}
+			});
+		} catch (DataAccessException daex) {
+			//EMPTY
+		}
 		return result;
 	}
 	
@@ -61,10 +68,31 @@ public class MockUtils {
 	 * @param clazz
 	 * @param list
 	 * @return Mock.
-	 * @throws DataAccessException
 	 */
-	public static <P, T extends EntitiesQuery<P>> T mockEntitiesQuery(Class<T> clazz, List<P> list) throws DataAccessException {
+	public static <P, T extends EntitiesQuery<P>> T mockEntitiesQuery(Class<T> clazz, List<P> list) {
 		return mockEntitiesQuery(clazz, list.iterator());
+	}
+	
+	/**
+	 * Mocks a {@link PersistenceWorker} 
+	 * 
+	 * @param onRead
+	 * @param onUpdate
+	 * @param onSave
+	 * @return Mock.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <P extends PersistentObject<?>>
+	PersistenceWorker<P> mockPw(P onRead, P onUpdate, P onSave) {
+		PersistenceWorker<P> pw = mock(PersistenceWorker.class);
+		try {
+			when(pw.read((P) any())).thenReturn(onRead);
+			when(pw.update((P) any())).thenReturn(onUpdate);
+			when(pw.store((P) any())).thenReturn(onSave);
+		} catch (DataException e) {
+			//EMPTY
+		}
+		return pw;
 	}
 
 }
