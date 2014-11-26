dd2e6fcf3049fc654c742f97bd0dfb8a96fbb3d7
Dimitris Kateros <katerosd@interamerican.gr>
2014-11-26 10:13:15 +0000

    skip tests
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3724 6845aa9c-c3df-f248-9547-742189ef8f90

--- releaseBo.sh
+++ releaseBo.sh
@@ -24,7 +24,7 @@ release=${snapshot/"-SNAPSHOT"/}
 
 echo "preparing release version $release"
 
-mvn -q --batch-mode release:prepare -Dbo2.skip.tests=true
+mvn -q --batch-mode release:prepare -Darguments="-DskipTests"
  
 e="$?"
 if [ $e -ne 0 ]
@@ -33,9 +33,7 @@ then
 	exit -1
 fi
 
-echo "performing release of version $release"
-
-mvn -q release:perform -Dbo2.skip.tests=true
+mvn -q release:perform -Darguments="-DskipTests"
 
 e="$?"
 if [ $e -ne 0 ]
