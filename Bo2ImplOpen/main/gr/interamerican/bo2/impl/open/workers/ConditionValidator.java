75fbd8ee4f805c9c81eedf249c65b4314af3c2b0
Spyros Nakos <nakoss@interamerican.gr>
2014-12-02 12:55:00 +0000

    BOTWO-27
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3758 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/workers/ConditionValidator.java
+++ Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/workers/ConditionValidator.java
@@ -3,6 +3,8 @@ package gr.interamerican.bo2.impl.open.workers;
 import gr.interamerican.bo2.arch.Rule;
 import gr.interamerican.bo2.arch.exceptions.DataException;
 import gr.interamerican.bo2.arch.exceptions.RuleException;
+import gr.interamerican.bo2.utils.attributes.Input;
+import gr.interamerican.bo2.utils.beans.MessagesBean;
 import gr.interamerican.bo2.utils.conditions.Condition;
 
 /**
@@ -13,18 +15,30 @@ import gr.interamerican.bo2.utils.conditions.Condition;
  */
 public class ConditionValidator<T> 
 extends AbstractResourceConsumer 
-implements Rule {
+implements Rule, Input<T> {
 	
 	
 
 	/**
-	 * Message to return if the validationfails.
-	 */
-	String message;
-	/**
 	 * Condition to check.
 	 */
 	Condition<T> condition;
+	
+	/**
+	 * Message to return if the validationfails.
+	 */
+	String messageKey;
+	
+	/**
+	 * Messages bean.
+	 */
+	MessagesBean messages;
+	
+	/**
+	 * 
+	 */
+	boolean failOn;
+
 	
 	/**
 	 * Object being validated.
@@ -32,21 +46,63 @@ implements Rule {
 	T validatedObject;
 	
 	/**
-	 * Creates a new ConditionValidator object. 
+	 * Creates a new ConditionValidator object.
+	 * 
+	 * @param condition
+	 *        Condition to check.
+	 * @param failOn 
+	 *        Defines when the Rule fails. A <code>true</code> value indicates
+	 *        that the rule throws an exception when the condition is met.
+	 *        A <code>false</code> value indicates that the rule throws an exception 
+	 *        when the condition is not met.
+	 * @param messages
+	 *        MessagesBean that fetches the messages.
+	 * @param messageKey 
+	 *        Message key used to fetch the key from the message bean.  
+	 */
+	public ConditionValidator
+	(Condition<T> condition, boolean failOn, MessagesBean messages, String messageKey) {
+		super();
+		this.messageKey = messageKey;
+		this.condition = condition;
+		this.messages = messages;
+		this.failOn = failOn;
+	}
+	
+	
+	/**
+	 * Creates a new ConditionValidator object.
+	 * 
+	 * The ConditionValidator throws an exception fails if the condition fails.  
 	 *
 	 * @param message
+	 *        Message for RuleExceptions 
 	 * @param condition
+	 *        Condition to check.
 	 */
 	public ConditionValidator(String message, Condition<T> condition) {
-		super();
-		this.message = message;
-		this.condition = condition;
+		this(condition, false, null, message);		
+	}
+	
+	
+	/**
+	 * Gets the message.
+	 * 
+	 * @return Returns the message.
+	 */
+	String getMessage() {
+		if (messages==null) {
+			return messageKey;
+		}
+		return messages.getString(messageKey, validatedObject); 
 	}
 	
 	@Override
 	public void apply() throws RuleException, DataException {
-		if (!condition.check(validatedObject)) {
-			throw new RuleException(message);
+		boolean result = condition.check(validatedObject);
+		boolean trigger = failOn ? result : !result;		
+		if (trigger) {			
+			throw new RuleException(getMessage());
 		}		
 	}
 
@@ -67,9 +123,11 @@ implements Rule {
 	public void setValidatedObject(T validatedObject) {
 		this.validatedObject = validatedObject;
 	}
-	
-	
-	
-	
+
+
+	@Override
+	public void setInput(T input) {
+		setValidatedObject(input);		
+	}	
 
 }
--- Bo2ImplOpen/test/gr/interamerican/bo2/impl/open/workers/TestConditionValidator.java
+++ Bo2ImplOpen/test/gr/interamerican/bo2/impl/open/workers/TestConditionValidator.java
@@ -1,7 +1,9 @@
 package gr.interamerican.bo2.impl.open.workers;
 
+import static org.mockito.Mockito.*;
 import gr.interamerican.bo2.arch.exceptions.DataException;
 import gr.interamerican.bo2.arch.exceptions.RuleException;
+import gr.interamerican.bo2.utils.beans.MessagesBean;
 import gr.interamerican.bo2.utils.conditions.Condition;
 
 import org.junit.Assert;
@@ -18,12 +20,32 @@ public class TestConditionValidator {
 	 */
 	@SuppressWarnings("unchecked")
 	@Test
-	public void testConstructor() {
+	public void testConstructor_1() {
 		String message = "X"; //$NON-NLS-1$
 		Condition<Object> condition= Mockito.mock(Condition.class);
 		ConditionValidator<Object> validator = 
 			new ConditionValidator<Object>(message, condition);
-		Assert.assertEquals(message, validator.message);
+		Assert.assertEquals(message, validator.messageKey);
+		Assert.assertNull(validator.messages);
+		Assert.assertFalse(validator.failOn);
+		Assert.assertEquals(condition, validator.condition);		
+	}
+	
+	/**
+	 * tests the constructor.
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testConstructor_2() {
+		String message = "X"; //$NON-NLS-1$
+		Condition<Object> condition= Mockito.mock(Condition.class);
+		MessagesBean bean = Mockito.mock(MessagesBean.class);
+		boolean failOn = true;
+		ConditionValidator<Object> validator = 
+			new ConditionValidator<Object>(condition, failOn, bean, message);
+		Assert.assertEquals(message, validator.messageKey);
+		Assert.assertEquals(bean, validator.messages);
+		Assert.assertEquals(failOn, validator.failOn);		
 		Assert.assertEquals(condition, validator.condition);		
 	}
 	
@@ -49,23 +71,127 @@ public class TestConditionValidator {
 	}
 	
 	/**
-	 * Tests the apply.
+	 * tests the constrsetValidatedObjectuctor.
+	 */	
+	@Test
+	public void testSetInput() {		
+		ConditionValidator<Object> validator = new ConditionValidator<Object>(null,null);
+		Object o = new Object();
+		validator.setInput(o);
+		Assert.assertEquals(o, validator.validatedObject);		
+	}
+	
+	/**
+	 * Tests the getMessage().
+	 * 
+	 */
+	@SuppressWarnings("nls")
+	@Test
+	public void testGetMessage() {
+		String expected = "foo";
+		Object o = new Object();
+		String key = "key";		
+		MessagesBean bean = mock(MessagesBean.class);
+		when(bean.getString(key, o)).thenReturn(expected);		
+		ConditionValidator<Object> validator = 
+			new ConditionValidator<Object>(null, true, bean, key);
+		validator.setInput(o);
+		String actual = validator.getMessage();
+		Assert.assertEquals(expected, actual);
+	}	
+	
+	/**
+	 * Tests the apply().
+	 * 
+	 * @throws DataException 
+	 * @throws RuleException 
+	 */
+	@SuppressWarnings("unchecked")
+	@Test(expected=RuleException.class)
+	public void testApply_failOnTrue_true() throws RuleException, DataException {
+		MessagesBean bean = mock(MessagesBean.class);
+		String key = "X"; //$NON-NLS-1$		
+		Condition<Object> condition= Mockito.mock(Condition.class);
+		Mockito.when(condition.check(Mockito.anyObject())).thenReturn(true);		
+		ConditionValidator<Object> validator = 
+			new ConditionValidator<Object>(condition, true, bean, key);
+		validator.setValidatedObject(new Object());
+		validator.apply();		
+	}
+	
+	/**
+	 * Tests the apply().
 	 * 
 	 * @throws DataException 
 	 * @throws RuleException 
 	 */
 	@SuppressWarnings("unchecked")
 	@Test
-	public void testApply_passing() throws RuleException, DataException {
-		String message = "X"; //$NON-NLS-1$
+	public void testApply_failOnTrue_false() throws RuleException, DataException {
+		MessagesBean bean = mock(MessagesBean.class);
+		String key = "X"; //$NON-NLS-1$		
 		Condition<Object> condition= Mockito.mock(Condition.class);
-		Mockito.when(condition.check(Mockito.anyObject())).thenReturn(true);
+		Mockito.when(condition.check(Mockito.anyObject())).thenReturn(false);		
 		ConditionValidator<Object> validator = 
-			new ConditionValidator<Object>(message, condition);
+			new ConditionValidator<Object>(condition, true, bean, key);
+		validator.setValidatedObject(new Object());
+		validator.apply();	
+		/* nothing should happen */
+	}
+	
+	/**
+	 * Tests apply().
+	 * 
+	 * @throws DataException 
+	 * @throws RuleException 
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testApply_failOnFalse_true() throws RuleException, DataException {
+		MessagesBean bean = mock(MessagesBean.class);
+		String key = "X"; //$NON-NLS-1$		
+		Condition<Object> condition= Mockito.mock(Condition.class);
+		Mockito.when(condition.check(Mockito.anyObject())).thenReturn(true);		
+		ConditionValidator<Object> validator = 
+			new ConditionValidator<Object>(condition, false, bean, key);
 		validator.setValidatedObject(new Object());
 		validator.apply();
-		/* no assertion, validationshould pass */		
+		/* nothing should happen */
 	}
+	
+	/**
+	 * Tests apply().
+	 * 
+	 * @throws DataException 
+	 * @throws RuleException 
+	 */
+	@SuppressWarnings("unchecked")
+	@Test(expected=RuleException.class)
+	public void testApply_failOnFalse_false() throws RuleException, DataException {
+		MessagesBean bean = mock(MessagesBean.class);
+		String key = "X"; //$NON-NLS-1$		
+		Condition<Object> condition= Mockito.mock(Condition.class);
+		Mockito.when(condition.check(Mockito.anyObject())).thenReturn(false);		
+		ConditionValidator<Object> validator = 
+			new ConditionValidator<Object>(condition, false, bean, key);
+		validator.setValidatedObject(new Object());
+		validator.apply();		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/**
 	 * Tests the apply.
