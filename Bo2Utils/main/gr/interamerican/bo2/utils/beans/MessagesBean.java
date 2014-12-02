8d77b3c308820b843a23d8cd7445d271b750fa04
Dimitris Kateros <katerosd@interamerican.gr>
2014-12-02 15:51:55 +0000

    BOTWO-29
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3766 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Utils/main/gr/interamerican/bo2/utils/beans/MessagesBean.java
+++ Bo2Utils/main/gr/interamerican/bo2/utils/beans/MessagesBean.java
@@ -12,51 +12,73 @@
  ******************************************************************************/
 package gr.interamerican.bo2.utils.beans;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 /**
- * 
+ * {@link ResourceBundle} facade.
  */
 public class MessagesBean {
-	
+
 	/**
 	 * ResourceBundle.
 	 */
 	ResourceBundle resourceBundle;
-
+	
 	/**
-	 * Creates a new Messages object. 
+	 * Creates a new Messages object.
 	 * 
-	 * @param resourceBundlePath 
-	 *        Path to a resource file in the class path that contains
-	 *        the resource bundle.
-	 *
+	 * @param resourceBundlePath
+	 *            Path to a resource file in the class path that contains the
+	 *            resource bundle.
 	 */
 	public MessagesBean(String resourceBundlePath) {
-		resourceBundle = ResourceBundle.getBundle(resourceBundlePath);		
+		this(resourceBundlePath, null);
+	}
+
+	/**
+	 * Creates a new Messages object.
+	 * 
+	 * @param resourceBundlePath
+	 *            Path to a resource file in the class path that contains the
+	 *            resource bundle.
+	 * @param locale
+	 *            Current {@link Locale}.
+	 */
+	public MessagesBean(String resourceBundlePath, Locale locale) {
+		if (locale != null) {
+			resourceBundle = ResourceBundle.getBundle(resourceBundlePath, locale, new XmlControl());
+		} else {
+			resourceBundle = ResourceBundle.getBundle(resourceBundlePath, new XmlControl());
+		}
 	}
 	
 	/**
-	 * Creates a new Messages object. 
+	 * Factory method. Uses the default system Locale.
 	 * 
-	 * @param resourceBundlePath 
-	 *        Path to a resource file in the class path that contains
-	 *        the resource bundle.
-	 * @param locale
-	 *        Current {@link Locale}. 
-	 *
+	 * @param resourceBundlePath
+	 *            Path to a resource file in the class path that contains the
+	 *            resource bundle.
+	 *            
+	 * @return MessagesBean instance.
 	 */
-	public MessagesBean(String resourceBundlePath, Locale locale) {
-		if(locale != null) {
-			resourceBundle = ResourceBundle.getBundle(resourceBundlePath, locale);
-		} else {
-			resourceBundle = ResourceBundle.getBundle(resourceBundlePath);
-		}
+	public static MessagesBean get(String resourceBundlePath) {
+		return new MessagesBean(resourceBundlePath, null);
 	}
-
+	
 	/**
 	 * Gets the string that corresponds to the specified key.
 	 * 
@@ -71,12 +93,12 @@ public class MessagesBean {
 			return '!' + key + '!';
 		}
 	}
-	
+
 	/**
 	 * Gets the string that corresponds to the specified key.
 	 * 
 	 * @param key
-	 * @param params 
+	 * @param params
 	 * @return String
 	 */
 	public String getString(String key, Object... params) {
@@ -88,6 +110,100 @@ public class MessagesBean {
 			return '!' + key + '!';
 		}
 	}
-	
+
+	/**
+	 * ResourceBundle.Control extension for working with XML files also.
+	 */
+	static class XmlControl extends ResourceBundle.Control {
+		
+		/**
+		 * xml format
+		 */
+		static final String XML = "xml"; //$NON-NLS-1$
+
+		@Override
+		public List<String> getFormats(String baseName) {
+			if (baseName == null) {
+				throw new NullPointerException();
+			}
+			List<String> standardFormats = super.getFormats(baseName);
+			List<String> allFormats = new ArrayList<String>(standardFormats);
+			allFormats.add(XML);
+			return allFormats;
+		}
+
+		@Override
+		public ResourceBundle newBundle(
+		String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+		throws IllegalAccessException, InstantiationException, IOException {
+			
+			if (baseName == null || locale == null || format == null || loader == null) {
+				throw new NullPointerException();
+			}
+			
+			ResourceBundle bundle = null;
+			if (format.equals(XML)) {
+				
+				String bundleName = toBundleName(baseName, locale);
+				String resourceName = toResourceName(bundleName, format);
+				InputStream stream = null;
+				
+				if (reload) {
+					URL url = loader.getResource(resourceName);
+					if (url != null) {
+						URLConnection connection = url.openConnection();
+						if (connection != null) {
+							connection.setUseCaches(false); // Disable caches to get fresh data for reloading.
+							stream = connection.getInputStream();
+						}
+					}
+				} else {
+					stream = loader.getResourceAsStream(resourceName);
+				}
+				if (stream != null) {
+					BufferedInputStream bis = new BufferedInputStream(stream);
+					bundle = new XMLResourceBundle(bis);
+					bis.close();
+				}
+			} else {
+				bundle = super.newBundle(baseName, locale, format, loader, reload);
+			}
+			return bundle;
+		}
+	}
+
+	/**
+	 * Loads an XML ResourceBundle
+	 */
+	static class XMLResourceBundle extends ResourceBundle {
+		
+		/**
+		 * properties
+		 */
+		private Properties props;
+
+		/**
+		 * Creates a new XMLResourceBundle object. 
+		 *
+		 * @param stream
+		 * @throws IOException
+		 */
+		XMLResourceBundle(InputStream stream) throws IOException {
+			props = new Properties();
+			props.loadFromXML(stream);
+		}
+
+		@Override
+		protected Object handleGetObject(String key) {
+			return props.getProperty(key);
+		}
+
+		@Override
+		public Enumeration<String> getKeys() {
+			Set<String> handleKeys = props.stringPropertyNames(); 
+			return Collections.enumeration(handleKeys); 
+		}
+
+	}
 
 }
--- Bo2Utils/test/gr/interamerican/bo2/utils/beans/TestMessagesBean.java
+++ Bo2Utils/test/gr/interamerican/bo2/utils/beans/TestMessagesBean.java
@@ -14,6 +14,11 @@ public class TestMessagesBean {
 	private static final String PATH = "gr.interamerican.bo2.utils.beans.MessagesBean"; //$NON-NLS-1$
 	
 	/**
+	 * Path to an XML resource bundle file.
+	 */
+	private static final String XML_PATH = "gr.interamerican.bo2.utils.beans.MessagesBeanXml"; //$NON-NLS-1$
+	
+	/**
 	 * Key of message 1.
 	 */
 	private static final String MSG1 = "message1"; //$NON-NLS-1$
@@ -54,7 +59,38 @@ public class TestMessagesBean {
 		MessagesBean bean = new MessagesBean(PATH);
 		String msg = bean.getString(MSG2,"string", 1); //$NON-NLS-1$
 		Assert.assertNotNull(msg);
-		
+	}
+	
+	/**
+	 * Tests getString(msg)
+	 */
+	@Test
+	public void testGetString_xml() {
+		MessagesBean bean = new MessagesBean(XML_PATH);
+		String msg = bean.getString(MSG1);
+		Assert.assertNotNull(msg);		
+	}
+	
+	/**
+	 * Tests getString(msg) when key is not found.
+	 */
+	@Test
+	public void testGetString_missingKey_xml() {
+		MessagesBean bean = new MessagesBean(XML_PATH);
+		String key = "nokey"; //$NON-NLS-1$
+		String expected = "!"+key+"!";  //$NON-NLS-1$//$NON-NLS-2$
+		String msg = bean.getString(key);
+		Assert.assertEquals(expected, msg);		
+	}
+	
+	/**
+	 * Tests getString(msg)
+	 */
+	@Test
+	public void testGetString_withParams_xml() {
+		MessagesBean bean = new MessagesBean(XML_PATH);
+		String msg = bean.getString(MSG2,"string", 1); //$NON-NLS-1$
+		Assert.assertNotNull(msg);
 	}
 
 }
