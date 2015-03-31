654104831755ebaa4c508ab7acc80427679331ff
Vasileios Mylonakis <milonakisv@interamerican.gr>
2015-03-31 14:34:37 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3937 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ArchUtils/test/gr/interamerican/bo2/Bo2MavenTestEnvironmentInitialization.java
+++ Bo2ArchUtils/test/gr/interamerican/bo2/Bo2MavenTestEnvironmentInitialization.java
@@ -1,6 +1,9 @@
 package gr.interamerican.bo2;
 
 
+import gr.interamerican.bo2.arch.utils.CacheRegistry;
+import gr.interamerican.bo2.arch.utils.beans.CacheImpl;
+
 import org.junit.Test;
 
 
@@ -8,16 +11,18 @@ import org.junit.Test;
  * Initialization class.
  */
 public class Bo2MavenTestEnvironmentInitialization {
-	
+
 	/**
 	 * for test
 	 */
 	public static boolean initialized = false;
-	
+
+
 	static {
+		CacheRegistry.registerCache("cache", new CacheImpl<Long>(), Long.class); //$NON-NLS-1$
 		initialized = true;
 	}
-	
+
 	/**
 	 * Empty test used in order to load the class from the test suite.
 	 */
