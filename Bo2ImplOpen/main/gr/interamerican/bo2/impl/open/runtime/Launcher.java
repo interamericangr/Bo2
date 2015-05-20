9519a8f0cd7d7178baeaeea210e202e8e741c7e0
Vasileios Mylonakis <milonakisv@interamerican.gr>
2015-05-20 08:55:00 +0000

    warnings
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3956 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/runtime/Launcher.java
+++ Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/runtime/Launcher.java
@@ -1,55 +1,38 @@
 /*******************************************************************************
- * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A. 
+ * Copyright (c) 2013 INTERAMERICAN PROPERTY AND CASUALTY INSURANCE COMPANY S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
- * 
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  ******************************************************************************/
 package gr.interamerican.bo2.impl.open.runtime;
 
-import gr.interamerican.bo2.arch.exceptions.DataException;
-import gr.interamerican.bo2.arch.exceptions.LogicException;
-import gr.interamerican.bo2.arch.exceptions.UnexpectedException;
 
 /**
  * The {@link Launcher}'s responsibility is to execute a {@link RuntimeCommand}. <br/>
- * 
+ *
  * This class has only a main method, that takes the name of a {@link RuntimeCommand} class as argument. Then it loads
  * the class, creates an instance of it and executes it.
- * 
+ *
  * @deprecated use {@link MultiLauncher} instead
  */
 @Deprecated
 public class Launcher {
-	
+
 	/**
 	 * Executes the command.
-	 * 
+	 *
 	 * @param args
 	 *            Only one argument is expected. The one and only argument must be the the name of a
 	 *            {@link RuntimeCommand} class. This class is loaded and then an instance of it is instantiated and
 	 *            executed.
-	 * 
-	 * @throws ClassNotFoundException
-	 *             If the class specified as argument is not found.
-	 * @throws IllegalAccessException
-	 *             At instantiation of the RuntimeCommand.
-	 * @throws InstantiationException
-	 *             At instantiation of the RuntimeCommand.
-	 * @throws LogicException
-	 *             Thrown by the RuntimeCommand.
-	 * @throws DataException
-	 *             Thrown by the RuntimeCommand.
-	 * @throws UnexpectedException
-	 *             Thrown by the RuntimeCommand.
 	 */
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
-			IllegalAccessException, UnexpectedException, DataException, LogicException {
+	public static void main(String[] args) {
 		launch(args);
 	}
 
@@ -59,7 +42,7 @@ public class Launcher {
 	 * need an exit value!=0 to denote an abnormal execution).<br>
 	 * Specifically when running this program using ant and setting to the java task 'fork="true"' this is the only way
 	 * for the ant process to exit.
-	 * 
+	 *
 	 * @param args
 	 */
 	private static void launch(String[] args) {
@@ -73,6 +56,6 @@ public class Launcher {
 			System.exit(-1);
 		}
 	}
-	
-	
+
+
 }
--- Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/runtime/MultiLauncher.java
+++ Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/runtime/MultiLauncher.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Lesser Public License v3
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/copyleft/lesser.html
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -12,16 +12,12 @@
  ******************************************************************************/
 package gr.interamerican.bo2.impl.open.runtime;
 
-import gr.interamerican.bo2.arch.exceptions.DataException;
-import gr.interamerican.bo2.arch.exceptions.LogicException;
-import gr.interamerican.bo2.arch.exceptions.UnexpectedException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * The {@link MultiLauncher}'s responsibility is to execute multiple {@link RuntimeCommand}. <br/>
- * 
+ *
  * This class has only a main method, that takes the name of a {@link RuntimeCommand} class as argument. Then it loads
  * the class, creates an instance of it and executes it.
  */
@@ -33,26 +29,12 @@ public class MultiLauncher {
 	private static final Logger LOG = LoggerFactory.getLogger(MultiLauncher.class);
 	/**
 	 * Executes a set of commands. If any of them fails, the jvm exits.
-	 * 
+	 *
 	 * @param args
 	 *            Multiple argument are expected. The arguments must be names of {@link RuntimeCommand}s class. This
 	 *            class are loaded and then an instance of it is instantiated and executed.
-	 * 
-	 * @throws ClassNotFoundException
-	 *             If the class specified as argument is not found.
-	 * @throws IllegalAccessException
-	 *             At instantiation of the RuntimeCommand.
-	 * @throws InstantiationException
-	 *             At instantiation of the RuntimeCommand.
-	 * @throws LogicException
-	 *             Thrown by the RuntimeCommand.
-	 * @throws DataException
-	 *             Thrown by the RuntimeCommand.
-	 * @throws UnexpectedException
-	 *             Thrown by the RuntimeCommand.
 	 */
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
-	IllegalAccessException, UnexpectedException, DataException, LogicException {
+	public static void main(String[] args) {
 		for (String className : args) {
 			launch(className);
 		}
@@ -64,7 +46,7 @@ public class MultiLauncher {
 	 * need an exit value!=0 to denote an abnormal execution).<br>
 	 * Specifically when running this program using ant and setting to the java task 'fork="true"' this is the only way
 	 * for the ant process to exit.
-	 * 
+	 *
 	 * @param className
 	 */
 	private static void launch(String className) {
