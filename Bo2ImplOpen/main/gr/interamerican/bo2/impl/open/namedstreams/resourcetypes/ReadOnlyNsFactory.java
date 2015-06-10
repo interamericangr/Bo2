39c488aea1c4f3e6815e80432b3f7ac50de17b06
Vasileios Mylonakis <milonakisv@interamerican.gr>
2015-06-10 14:41:00 +0000

    warnungs
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@3991 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/namedstreams/resourcetypes/ReadOnlyNsFactory.java
+++ Bo2ImplOpen/main/gr/interamerican/bo2/impl/open/namedstreams/resourcetypes/ReadOnlyNsFactory.java
@@ -19,21 +19,21 @@ import java.nio.charset.Charset;
 
 /**
  * {@link NamedStreamFactory} for read only resource types.
- * 
+ *
  * This type of factory can only create {@link NamedInputStream}s and
  * {@link NamedBufferedReader}s.
  */
-public abstract class ReadOnlyNsFactory 
+public abstract class ReadOnlyNsFactory
 implements NamedStreamFactory {
-	
+
 	/**
-	 * ResourceType of this factory. 
+	 * ResourceType of this factory.
 	 */
 	StreamResource resourceType;
 
 	/**
 	 * Creates a new ReadOnlyNsFactory.
-	 * 
+	 *
 	 * @param resourceType
 	 */
 	public ReadOnlyNsFactory(StreamResource resourceType) {
@@ -43,15 +43,15 @@ implements NamedStreamFactory {
 
 	@Override
 	public NamedStream<?> create(NamedStreamDefinition def)
-	throws CouldNotCreateNamedStreamException {	
+			throws CouldNotCreateNamedStreamException {
 		onCreate(def.getResourceType(), resourceType);
 		return createNs(def);
 	}
-	
+
 	@Override
 	public NamedStream<?> convert(NamedStream<?> ns, StreamType type, String name)
-	throws CouldNotConvertNamedStreamException {
-		onConvert(ns.getResourceType(), resourceType);		
+			throws CouldNotConvertNamedStreamException {
+		onConvert(ns.getResourceType(), resourceType);
 		NamedStreamDefinition nsd = new NamedStreamDefinition();
 		ReflectionUtils.copyProperties(ns, nsd);
 		nsd.setName(name);
@@ -63,77 +63,77 @@ implements NamedStreamFactory {
 			throw new CouldNotConvertNamedStreamException(e);
 		}
 	}
-	
+
 	/**
 	 * Creates an InputStream according to the specified {@link NamedStreamDefinition}.
-	 * 
+	 *
 	 * @param def
 	 *        NamedStreamDefinition with the necessary information for the
-	 *        creation of the stream. 
-	 * 
+	 *        creation of the stream.
+	 *
 	 * @return Returns the InputStream.
 	 * @throws CouldNotCreateNamedStreamException
 	 */
 	protected abstract InputStream openInputStream(NamedStreamDefinition def)
-	throws CouldNotCreateNamedStreamException;
-	
+			throws CouldNotCreateNamedStreamException;
+
 	/**
 	 * Creates a new NamedStream.
-	 * 
+	 *
 	 * @param def
-	 * 
+	 *
 	 * @return Returns the {@link NamedStream}.
-	 * @throws IOException
-	 * @throws CouldNotConvertNamedStreamException 
+	 * @throws CouldNotCreateNamedStreamException
 	 */
-	NamedStream<?> createNs(NamedStreamDefinition def) 
-	throws CouldNotCreateNamedStreamException {
+	NamedStream<?> createNs(NamedStreamDefinition def)
+			throws CouldNotCreateNamedStreamException {
 		InputStream stream = openInputStream(def);
-		return createNs(stream, def);		
+		return createNs(stream, def);
 	}
-	
-	
+
+
 	/**
 	 * Creates a new NamedStream.
-	 * 
-	 * @param file
+	 *
+	 * @param stream
+	 *
 	 * @param def
-	 * 
+	 *
 	 * @return Returns the {@link NamedStream}.
-	 * @throws IOException
+	 * @throws CouldNotCreateNamedStreamException
 	 */
-	NamedStream<?> createNs(InputStream stream, NamedStreamDefinition def) 
-	throws CouldNotCreateNamedStreamException {				
-		StreamType type = def.getType();		
-		switch (type) {		
+	NamedStream<?> createNs(InputStream stream, NamedStreamDefinition def)
+			throws CouldNotCreateNamedStreamException {
+		StreamType type = def.getType();
+		switch (type) {
 		case BUFFEREDREADER:
-			return reader(stream, def.getName(), def.getEncoding(), def.getUri());			
+			return reader(stream, def.getName(), def.getEncoding(), def.getUri());
 		case INPUTSTREAM:
-			return input(stream, def.getName(), def.getRecordLength(), def.getEncoding(), def.getUri());			
+			return input(stream, def.getName(), def.getRecordLength(), def.getEncoding(), def.getUri());
 		default:
 			String msg = "Invalid NamedStream type " + StringUtils.toString(type); //$NON-NLS-1$
-			throw new CouldNotCreateNamedStreamException(msg);			
-		}		
+			throw new CouldNotCreateNamedStreamException(msg);
+		}
 	}
-	
+
 	/**
 	 * Creates a new NamedBufferedReader that wraps a file.
-	 * 
+	 *
 	 * The specified file provides the underlying resource of the NamedStream.
-	 * 
+	 *
 	 * The file is read using the user-defined encoding in the Bo2 configuration.
-	 * 
-	 * @param file
-	 *        File.
+	 *
+	 * @param stream
+	 *            stream.
 	 * @param name
-	 *        Stream name.
+	 *            Stream name.
 	 * @param encoding
-	 *        Encoding (if applicable).
+	 *            Encoding (if applicable).
 	 * @param uri
-	 *        Stream URI.         
-	 * 
+	 *            Stream URI.
+	 *
 	 * @return Returns the NamedBufferedReader.
-	 * 
+	 *
 	 * @throws IOException
 	 */
 	NamedBufferedReader reader(InputStream stream, String name, Charset encoding, String uri) {
@@ -141,29 +141,29 @@ implements NamedStreamFactory {
 		BufferedReader br = new BufferedReader(insr);
 		return new NamedBufferedReader(resourceType, br, name, stream, encoding, uri);
 	}
-	
+
 	/**
 	 * Creates a new NamedInputStream that wraps a file.
-	 * 
+	 *
 	 * The specified file provides the underlying resource of the NamedStream.
-	 * 
-	 * @param file
-	 *        File.
+	 *
+	 * @param stream
+	 *            stream.
 	 * @param name
-	 *        Stream name.
+	 *            Stream name.
 	 * @param recordLength
-	 *        Record length.
+	 *            Record length.
 	 * @param encoding
-	 *        Encoding (if applicable).
+	 *            Encoding (if applicable).
 	 * @param uri
-	 *        Stream URI.         
-	 * 
+	 *            Stream URI.
+	 *
 	 * @return Returns the NamedInputStream.
 	 * @throws FileNotFoundException
 	 */
-	NamedInputStream input(InputStream stream, String name, int recordLength, Charset encoding, String uri) {		
+	NamedInputStream input(InputStream stream, String name, int recordLength, Charset encoding, String uri) {
 		return new NamedInputStream(resourceType, stream, name, recordLength, stream, encoding, uri);
 	}
-	
+
 
 }
