7217bffb607b157bbfd81653a783a906033083e2
Vasileios Mylonakis <milonakisv@interamerican.gr>
2015-07-21 09:20:17 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@4045 6845aa9c-c3df-f248-9547-742189ef8f90

---
+++ Bo2Creation/main/gr/interamerican/bo2/creation/creators/GlobalFixtureResolver.java
@@ -0,0 +1,44 @@
+package gr.interamerican.bo2.creation.creators;
+
+import gr.interamerican.bo2.creation.FixtureResolver;
+import gr.interamerican.bo2.creation.ObjectFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Global implementation of {@link FixtureResolver} <br/>
+ * It uses a final Map (Concurrent) that will be available on all Threads in the same JVM.
+ */
+public class GlobalFixtureResolver implements FixtureResolver {
+
+	/**
+	 * tlCache
+	 */
+	final ConcurrentHashMap<Class<?>, Object> cache = new ConcurrentHashMap<Class<?>, Object>();
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <M> M resolveFixture(Class<M> type) {
+		Object fixture = cache.get(type);
+		if (fixture instanceof ObjectFactory) {
+			ObjectFactory fixtureFactory = (ObjectFactory) fixture;
+			return fixtureFactory.create(type);
+		}
+		return (M) cache.get(type);
+	}
+
+	@Override
+	public void clearFixturesCache() {
+		cache.clear();
+	}
+
+	@Override
+	public <M> void registerFixture(Class<M> declarationType, M fixture) {
+		cache.putIfAbsent(declarationType, fixture);
+	}
+
+	public <M> void registerFixture(Class<M> declarationType, ObjectFactory fixtureFactory) {
+		cache.putIfAbsent(declarationType, fixtureFactory);
+	}
+
+}
