3e35001d2b586f4de86dfd7b1f544abd7d0c6161
Vasileios Mylonakis <milonakisv@interamerican.gr>
2015-07-28 12:54:05 +0000

    javadoc
    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@4056 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2Quartz/main/gr/interamerican/bo2/quartz/runtime/MemorySetting.java
+++ Bo2Quartz/main/gr/interamerican/bo2/quartz/runtime/MemorySetting.java
@@ -7,31 +7,34 @@ public interface MemorySetting {
 
 	/**
 	 * @param m
+	 *            minimum memory setting (in MB)
 	 */
 	void setMinMemory(Long m);
 
 	/**
-	 * @return min memory
+	 * @return minimum memory setting (in MB)
 	 */
 	Long getMinMemory();
 
 	/**
 	 * @param m
+	 *            maximum memory setting (in MB)
 	 */
 	void setMaxMemory(Long m);
 
 	/**
-	 * @return max memory
+	 * @return maximum memory setting (in MB)
 	 */
 	Long getMaxMemory();
 
 	/**
 	 * @param pg
+	 *            permanent generation setting (in MB)
 	 */
 	void setPermGen(Long pg);
 
 	/**
-	 * @return perm gen
+	 * @return permanent generation setting (in MB)
 	 */
 	Long getPermGen();
 }
