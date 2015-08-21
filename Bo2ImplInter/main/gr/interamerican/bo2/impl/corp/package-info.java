6193eef622c3db4b9f720f76aae245addd9daf0f
Dimitris Kateros <katerosd@interamerican.gr>
2015-08-21 12:58:35 +0000

    git-svn-id: http://svn-srv.interamerican.gr/svn/bo2/trunk/Bo2@4071 6845aa9c-c3df-f248-9547-742189ef8f90

--- Bo2ImplInter/main/gr/interamerican/bo2/impl/corp/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains implementations of the architecture 
- * defined in the gr.interamerican.bo2.arch package based
- * on technologies built and used specifically by Interamerican Greece.
- * 
- */
- package gr.interamerican.bo2.impl.corp;
-
--- Bo2ImplInter/main/gr/interamerican/bo2/impl/corp/utils/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains various utility classes.
- * 
- */
- package gr.interamerican.bo2.impl.corp.utils;
-
